a0fe2dfd71f987dc88eb442cbbcfbfbc22674449
David <davidaase@hotmail.com>
2017-05-30 00:08:17 +0200

    [core] Make jetty async (not sure if good?)

--- src/main/java/io/javalin/embeddedserver/jetty/JettyHandler.java
+++ src/main/java/io/javalin/embeddedserver/jetty/JettyHandler.java
@@ -8,7 +8,12 @@
 package io.javalin.embeddedserver.jetty;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,17 +28,34 @@ public class JettyHandler extends SessionHandler {
 
     private JavalinServlet javalinServlet;
 
+    private ThreadPoolExecutor executor;
+
     public JettyHandler(JavalinServlet javalinServlet) {
         this.javalinServlet = javalinServlet;
+        this.executor = new ThreadPoolExecutor(200, 200, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        executor.allowCoreThreadTimeOut(true);
     }
 
     @Override
     public void doHandle(String target, Request jettyRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
         CachedRequestWrapper cachedRequest = new CachedRequestWrapper(request);
         cachedRequest.setAttribute("jetty-target", target);
         cachedRequest.setAttribute("jetty-request", jettyRequest);
-        javalinServlet.service(cachedRequest, response);
-        jettyRequest.setHandled(true);
+
+        AsyncContext asyncContext = request.startAsync();
+        CompletableFuture.runAsync(() -> {
+            try {
+                javalinServlet.service(cachedRequest, response);
+                request.getAsyncContext().complete();
+            } catch (IOException | ServletException e) {
+                throw new RuntimeException(e);
+            }
+        }, executor).thenAccept((Void) -> {
+            jettyRequest.setHandled(true);
+            asyncContext.complete();
+        });
+
     }
 
 }
--- src/test/java/io/javalin/TestAsync.java
+++ src/test/java/io/javalin/TestAsync.java
@@ -6,62 +6,50 @@
 
 package io.javalin;
 
-import java.util.concurrent.ForkJoinPool;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.javalin.embeddedserver.jetty.EmbeddedJettyFactory;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-
 public class TestAsync {
 
-    @Test
-    @Ignore("Just for running manually")
-    public void test_async() throws Exception {
+    private static Logger log = LoggerFactory.getLogger(TestAsync.class);
+
+    public static void main(String[] args) throws Exception {
 
         AtomicInteger requestNum = new AtomicInteger(0);
 
         Javalin app = Javalin.create()
-            .embeddedServer(new EmbeddedJettyFactory(() -> new Server(new QueuedThreadPool(16, 10, 60_000))))
+            .embeddedServer(new EmbeddedJettyFactory(() -> new Server(new QueuedThreadPool(12, 8, 60_000))))
             .port(5454)
             .start()
             .awaitInitialization();
 
         app.get("/", (req, res) -> {
             int num = requestNum.getAndIncrement();
-            System.out.println("Threads:" + app.embeddedServer().activeThreadCount() + " - Request #" + num);
+            log.info(timeStamp() + "Threads:" + app.embeddedServer().activeThreadCount() + " - Request #" + num);
             try {
-                Thread.sleep(5_000);
+                Thread.sleep(500);
                 res.body("res");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
-        long startTime = System.currentTimeMillis();
-        ForkJoinPool forkJoinPool = new ForkJoinPool(100);
-        forkJoinPool.submit(
-            () -> IntStream.range(0, 50).parallel().forEach(i -> {
-                try {
-                    System.out.println("Call #" + i);
-                    call(5454, "/");
-                } catch (UnirestException e) {
-                    e.printStackTrace();
-                }
-            })
-        ).get();
-        System.out.println("took " + (System.currentTimeMillis() - startTime) + " milliseconds");
     }
 
-    private String call(int port, String path) throws UnirestException {
-        return Unirest.get("http://localhost:" + port + path).asString().getBody();
+    private static String timeStamp() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:MM:SS")) + ": ";
     }
+
+    // run tests using node and artillery
+    // npm install -g artillery
+    // artillery quick --duration 60 --rate 10 -n 20 http://localhost:5454
 
 }
