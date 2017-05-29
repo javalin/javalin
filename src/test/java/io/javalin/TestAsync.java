3893e37f556d3f3d09b7d95b1aaecf85d2cd4647
David <davidaase@hotmail.com>
2017-05-30 00:12:10 +0200

    Revert "[core] Make jetty async (not sure if good?)"
    This reverts commit a0fe2dfd71f987dc88eb442cbbcfbfbc22674449.

--- src/test/java/io/javalin/TestAsync.java
+++ src/test/java/io/javalin/TestAsync.java
@@ -6,50 +6,62 @@
 
 package io.javalin;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import io.javalin.embeddedserver.jetty.EmbeddedJettyFactory;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 public class TestAsync {
 
-    private static Logger log = LoggerFactory.getLogger(TestAsync.class);
-
-    public static void main(String[] args) throws Exception {
+    @Test
+    @Ignore("Just for running manually")
+    public void test_async() throws Exception {
 
         AtomicInteger requestNum = new AtomicInteger(0);
 
         Javalin app = Javalin.create()
-            .embeddedServer(new EmbeddedJettyFactory(() -> new Server(new QueuedThreadPool(12, 8, 60_000))))
+            .embeddedServer(new EmbeddedJettyFactory(() -> new Server(new QueuedThreadPool(16, 10, 60_000))))
             .port(5454)
             .start()
             .awaitInitialization();
 
         app.get("/", (req, res) -> {
             int num = requestNum.getAndIncrement();
-            log.info(timeStamp() + "Threads:" + app.embeddedServer().activeThreadCount() + " - Request #" + num);
+            System.out.println("Threads:" + app.embeddedServer().activeThreadCount() + " - Request #" + num);
             try {
-                Thread.sleep(500);
+                Thread.sleep(5_000);
                 res.body("res");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
+        long startTime = System.currentTimeMillis();
+        ForkJoinPool forkJoinPool = new ForkJoinPool(100);
+        forkJoinPool.submit(
+            () -> IntStream.range(0, 50).parallel().forEach(i -> {
+                try {
+                    System.out.println("Call #" + i);
+                    call(5454, "/");
+                } catch (UnirestException e) {
+                    e.printStackTrace();
+                }
+            })
+        ).get();
+        System.out.println("took " + (System.currentTimeMillis() - startTime) + " milliseconds");
     }
 
-    private static String timeStamp() {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:MM:SS")) + ": ";
+    private String call(int port, String path) throws UnirestException {
+        return Unirest.get("http://localhost:" + port + path).asString().getBody();
     }
-
-    // run tests using node and artillery
-    // npm install -g artillery
-    // artillery quick --duration 60 --rate 10 -n 20 http://localhost:5454
 
 }
