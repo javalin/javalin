fbf8190da053a2479f99ffb210db89a49170878f
David <davidaase@hotmail.com>
2017-06-02 02:26:23 +0200

    Port embedded servers interfaces

---
+++ src/main/kotlin/io.javalin/embeddedserver/EmbeddedServer.kt
@@ -0,0 +1,22 @@
+/*
+ * Javalin - https://javalin.io
+ * Copyright 2017 David Åse
+ * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ */
+
+package io.javalin.embeddedserver
+
+interface EmbeddedServer {
+
+    @Throws(Exception::class)
+    fun start(host: String, port: Int): Int
+
+    @Throws(InterruptedException::class)
+    fun join()
+
+    fun stop()
+
+    fun activeThreadCount(): Int
+
+    fun attribute(key: String): Any
+}
