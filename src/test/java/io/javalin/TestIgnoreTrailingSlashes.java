9c3a3b446fce6f770972555e6d8e2076533f5537
enchanting <enchantinggg4@gmail.com>
2017-08-03 10:26:59 +0300

    Fixed some code, tests now run.

--- src/test/java/io/javalin/TestIgnoreTrailingSlashes.java
+++ src/test/java/io/javalin/TestIgnoreTrailingSlashes.java
@@ -16,25 +16,25 @@ import static org.hamcrest.Matchers.not;
  */
 public class TestIgnoreTrailingSlashes {
 
-    @Test(expected = IOException.class)
+    @Test()
     public void init() throws IOException {
 
         String endpointResponse = "Hello, slash!";
         String endpoint = "/hello";
-
+        int port = 7787;
         SimpleHttpClient simpleHttpClient = new SimpleHttpClient();
         Javalin app = Javalin.create()
-                .port(7777)
+                .port(port)
                 .start();
 
         app.setTrailingSlashesIgnored(true);
 
         app.get(endpoint, (ctx) -> ctx.result(endpointResponse));
-        assertThat(simpleHttpClient.http_GET("localhost" + endpoint).getBody(), is(endpointResponse));
-        assertThat(simpleHttpClient.http_GET("localhost" + endpoint + "/").getBody(), is(endpointResponse));
+        assertThat(simpleHttpClient.http_GET("http://localhost:" + port + endpoint).getBody(), is(endpointResponse));
+        assertThat(simpleHttpClient.http_GET("http://localhost:" + port + endpoint + "/").getBody(), is(endpointResponse));
         app.setTrailingSlashesIgnored(false);
-        assertThat(simpleHttpClient.http_GET("localhost" + endpoint).getBody(), is(endpointResponse));
-        assertThat(simpleHttpClient.http_GET("localhost" + endpoint + "/").getBody(), not(endpointResponse));
+        assertThat(simpleHttpClient.http_GET("http://localhost:" + port + endpoint).getBody(), is(endpointResponse));
+        assertThat(simpleHttpClient.http_GET("http://localhost:" + port + endpoint + "/").getBody(), not(endpointResponse));
 
         app.stop();
     }
