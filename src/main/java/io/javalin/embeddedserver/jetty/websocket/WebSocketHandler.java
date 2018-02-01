101ea4637622dd3b609862ce3208c5249bc93467
plombardi <plombardi89@gmail.com>
2018-02-01 17:06:11 -0500

    Drop getSessionId() as it is rather silly.

--- src/main/java/io/javalin/embeddedserver/jetty/websocket/WebSocketHandler.java
+++ src/main/java/io/javalin/embeddedserver/jetty/websocket/WebSocketHandler.java
@@ -20,6 +20,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -51,19 +52,26 @@ public class WebSocketHandler {
         this.errorHandler = errorHandler;
     }
 
-    public Collection<WsSession> getSessions() {
+    /**
+     * Returns all open sessions at the time of invocation. If you intend to use this method to build a client broadcast
+     * capability then you should handle connection errors due to difference in time between retrieval and
+     * {@link WsSession#send(String)}.
+     *
+     * @return a collection of {@link WsSession}
+     */
+    public Set<WsSession> getSessions() {
         return sessions.keySet()
                 .stream()
                 .filter(Session::isOpen)
                 .map(this::createWsSession)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
     }
 
     // Jetty annotations
 
     @OnWebSocketConnect
     public void _internalOnConnectProxy(Session session) throws Exception {
-        String sessionId = registerSession(session);
+        registerSession(session);
         if (connectHandler != null) {
             connectHandler.handle(createWsSession(session));
         }
@@ -71,7 +79,7 @@ public class WebSocketHandler {
 
     @OnWebSocketMessage
     public void _internalOnMessageProxy(Session session, String message) throws Exception {
-        String sessionId = registerSession(session);
+        registerSession(session);
         if (messageHandler != null) {
             messageHandler.handle(createWsSession(session), message);
         }
@@ -79,36 +87,30 @@ public class WebSocketHandler {
 
     @OnWebSocketClose
     public void _internalOnCloseProxy(Session session, int statusCode, String reason) throws Exception {
-        String sessionId = registerSession(session);
         if (closeHandler != null) {
             closeHandler.handle(createWsSession(session), statusCode, reason);
         }
-
-        sessions.remove(session);
+        unregisterSession(session);
     }
 
     @OnWebSocketError
     public void _internalOnErrorProxy(Session session, Throwable throwable) throws Exception {
-        String sessionId = registerSession(session);
+       registerSession(session);
         if (errorHandler != null) {
             errorHandler.handle(createWsSession(session), throwable);
         }
     }
 
-    private String registerSession(Session session) {
-        return Objects.requireNonNull(sessions.computeIfAbsent(session, (s) -> nextSessionId()));
+    private void registerSession(Session session) {
+        sessions.putIfAbsent(session, nextSessionId());
     }
 
     private void unregisterSession(Session session) {
-        sessions.remove(Objects.requireNonNull(session));
+        sessions.remove(session);
     }
 
     private WsSession createWsSession(Session session) {
-        return new WsSession(getSessionId(session), session);
-    }
-
-    private String getSessionId(Session session) {
-        return sessions.get(session);
+        return new WsSession(sessions.get(session), session);
     }
 
     private String nextSessionId() {
