7d5226d0da0fedcea8e8cf366fb2696713247640
Andrei Shikov <andreiishikov@gmail.com>
2018-02-20 01:46:31 +0200

    Use stream for response string output

--- src/main/java/io/javalin/core/JavalinServlet.kt
+++ src/main/java/io/javalin/core/JavalinServlet.kt
@@ -15,12 +15,13 @@ import io.javalin.embeddedserver.CachedRequestWrapper
 import io.javalin.embeddedserver.CachedResponseWrapper
 import io.javalin.embeddedserver.StaticResourceHandler
 import org.slf4j.LoggerFactory
-import java.io.ByteArrayInputStream
+import java.io.InputStream
 import java.util.zip.GZIPOutputStream
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import kotlin.properties.Delegates
 
 class JavalinServlet(
         val contextPath: String,
@@ -36,17 +37,17 @@ class JavalinServlet(
 
     private val log = LoggerFactory.getLogger(JavalinServlet::class.java)
 
-    var staticResourceHandler: StaticResourceHandler? = null
+    var staticResourceHandler: StaticResourceHandler by Delegates.notNull()
 
     fun service(servletRequest: ServletRequest, servletResponse: ServletResponse) {
 
         val req = CachedRequestWrapper(servletRequest as HttpServletRequest, maxRequestCacheBodySize) // cached for reading multiple times
-        val res =
-                if (logLevel == LogLevel.EXTENSIVE) CachedResponseWrapper(servletResponse as HttpServletResponse) // body needs to be copied for logging
-                else servletResponse as HttpServletResponse
+        val res = if (logLevel == LogLevel.EXTENSIVE) CachedResponseWrapper(servletResponse as HttpServletResponse) // body needs to be copied for logging
+                  else servletResponse as HttpServletResponse
         val type = HandlerType.fromServletRequest(req)
         val requestUri = req.requestURI
         val ctx = ContextUtil.create(res, req)
+        fun tryWithMapper(f: () -> Unit) = exceptionMapper.catchException(ctx, f)
 
         ctx.header("Server", "Javalin")
         ctx.attribute("javalin-request-log-start-time", System.nanoTime())
@@ -54,61 +55,50 @@ class JavalinServlet(
         res.characterEncoding = defaultCharacterEncoding
         res.contentType = defaultContentType
 
-        try { // before-handlers, endpoint-handlers, static-files
-
-            for (beforeEntry in matcher.findEntries(HandlerType.BEFORE, requestUri)) {
-                beforeEntry.handler.handle(ContextUtil.update(ctx, beforeEntry, requestUri))
+        tryWithMapper {
+            // before-handlers
+            matcher.findEntries(HandlerType.BEFORE, requestUri).forEach { entry ->
+                entry.handler.handle(ContextUtil.update(ctx, entry, requestUri))
             }
 
-            val entries = matcher.findEntries(type, requestUri)
-            if (!entries.isEmpty()) {
-                for (endpointEntry in entries) {
-                    endpointEntry.handler.handle(ContextUtil.update(ctx, endpointEntry, requestUri))
-                    if (!ctx.nexted()) {
-                        break
-                    }
+            // endpoint-handlers, static-files
+            val endpointEntries = matcher.findEntries(type, requestUri)
+            endpointEntries.forEach { entry ->
+                entry.handler.handle(ContextUtil.update(ctx, entry, requestUri))
+                if (!ctx.nexted()) {
+                    return@tryWithMapper
                 }
-            } else if (type !== HandlerType.HEAD || type === HandlerType.HEAD && matcher.findEntries(HandlerType.GET, requestUri).isEmpty()) {
-                if (!staticResourceHandler!!.handle(req, res)) {
+            }
+
+
+            if (endpointEntries.isEmpty() && type != HandlerType.HEAD || (type == HandlerType.HEAD && matcher.findEntries(HandlerType.GET, requestUri).isEmpty())) {
+                if (!staticResourceHandler.handle(req, res)) {
                     throw HaltException(404, "Not found")
                 }
             }
 
-        } catch (e: Exception) {
-            // both before-handlers and endpoint-handlers can throw Exception,
-            // we need to handle these exceptions here in order to run after-handlers even if an exception was thrown
-            exceptionMapper.handle(e, ctx)
         }
 
-        try { // after-handlers
-            for (afterEntry in matcher.findEntries(HandlerType.AFTER, requestUri)) {
-                afterEntry.handler.handle(ContextUtil.update(ctx, afterEntry, requestUri))
+        tryWithMapper {
+            // after-handlers
+            matcher.findEntries(HandlerType.AFTER, requestUri).forEach { entry ->
+                entry.handler.handle(ContextUtil.update(ctx, entry, requestUri))
             }
-        } catch (e: Exception) {
-            // after-handlers can also throw exceptions
-            exceptionMapper.handle(e, ctx)
         }
 
-        try { // error mapping (turning status codes into standardized messages/pages)
+        tryWithMapper {
+            // error mapping (turning status codes into standardized messages/pages)
             errorMapper.handle(ctx.status(), ctx)
-        } catch (e: RuntimeException) {
-            // depressingly, the error mapping itself could throw a runtime exception
-            // we need to handle these last... but that's it.
-            exceptionMapper.handle(e, ctx)
         }
 
         // write result to servlet-response (if not already committed)
-        val doGzip = gzipShouldBeDone(ctx.resultString(), req)
+        val doGzip = gzipShouldBeDone(ctx.resultStream(), req)
         if (!res.isCommitted) {
-            ctx.resultString()?.let { resultString ->
-                ctx.result(ByteArrayInputStream(resultString.toByteArray()))
-            }
             ctx.resultStream()?.let { resultStream ->
                 if (doGzip) {
-                    GZIPOutputStream(res.outputStream, true).let { gzippedStream ->
+                    GZIPOutputStream(res.outputStream, true).use { gzippedStream ->
                         res.setHeader(Header.CONTENT_ENCODING, "gzip")
                         resultStream.copyTo(gzippedStream)
-                        gzippedStream.close()
                     }
                 } else {
                     resultStream.copyTo(res.outputStream)
@@ -121,8 +111,8 @@ class JavalinServlet(
 
     }
 
-    private fun gzipShouldBeDone(resultString: String?, req: CachedRequestWrapper) = dynamicGzipEnabled
-            && (resultString ?: "").length > 1500 // mtu is apparently ~1500 bytes
+    private fun gzipShouldBeDone(resultStream: InputStream?, req: CachedRequestWrapper) = dynamicGzipEnabled
+            && resultStream?.available() ?: 0 > 1500 // mtu is apparently ~1500 bytes
             && (req.getHeader(Header.ACCEPT_ENCODING) ?: "").contains("gzip", ignoreCase = true)
 
 }
