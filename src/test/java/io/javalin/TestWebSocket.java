e66583bba175d672896279bfeb33f41f63af6747
David <davidaase@hotmail.com>
2018-03-18 12:50:12 +0100

    [test] Test websocket paths for unicode params

--- src/test/java/io/javalin/TestWebSocket.java
+++ src/test/java/io/javalin/TestWebSocket.java
@@ -160,17 +160,21 @@ public class TestWebSocket {
         doAndSleepWhile(testClient1_1::connect, () -> !testClient1_1.isOpen());
         doAndSleepWhile(testClient1_1::close, testClient1_1::isClosing);
 
-        TestClient testClient1_2 = new TestClient(URI.create("ws://localhost:" + app.port() + "/websocket/params/this/should/not/work"));
+        TestClient testClient1_2 = new TestClient(URI.create("ws://localhost:" + app.port() + "/websocket/params/%E2%99%94"));
         doAndSleepWhile(testClient1_2::connect, () -> !testClient1_2.isOpen());
         doAndSleepWhile(testClient1_2::close, testClient1_2::isClosing);
+
+        TestClient testClient1_3 = new TestClient(URI.create("ws://localhost:" + app.port() + "/websocket/params/this/should/not/work"));
+        doAndSleepWhile(testClient1_3::connect, () -> !testClient1_3.isOpen());
+        doAndSleepWhile(testClient1_3::close, testClient1_3::isClosing);
 
         TestClient testClient2_1 = new TestClient(URI.create("ws://localhost:" + app.port() + "/websocket/params/another/test/long/path"));
         doAndSleepWhile(testClient2_1::connect, () -> !testClient2_1.isOpen());
         doAndSleepWhile(testClient2_1::close, testClient2_1::isClosing);
 
-
         assertThat(log, containsInAnyOrder(
             "one",
+            "♔",
             "another long path"
         ));
         app.stop();
