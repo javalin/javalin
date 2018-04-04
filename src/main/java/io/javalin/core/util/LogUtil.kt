6a7f4590654f8f7bb01cdc5b0fd1fc6d21034e02
Christian Neverdal <flyrev@gmail.com>
2018-04-04 20:34:24 +0200

    Have default branch for LogLevel.OFF

--- src/main/java/io/javalin/core/util/LogUtil.kt
+++ src/main/java/io/javalin/core/util/LogUtil.kt
@@ -46,6 +46,7 @@ object LogUtil {
                         |${if (resBody.isNotEmpty()) (if (gzipped) "dynamically gzipped response ..." else resBody) else "No body was set"}
                         |----------------------------------------------------------------------------------""".trimMargin())
                 }
+                LogLevel.OFF -> {}
             }
         }
     }
