cde2bf520205d190269784422641a3bb67d10876
Sidd <iProdigy@users.noreply.github.com>
2018-04-14 23:54:58 +0000

    [core] Simplify TemplateUtil#model and Bump Kotlin (#194)

--- src/main/java/io/javalin/translator/template/TemplateUtil.kt
+++ src/main/java/io/javalin/translator/template/TemplateUtil.kt
@@ -6,24 +6,12 @@
 
 package io.javalin.translator.template
 
-import java.util.*
-
 object TemplateUtil {
     @JvmStatic
     fun model(vararg args: Any?): Map<String, Any?> {
         if (args.size % 2 != 0) {
             throw IllegalArgumentException("Number of arguments must be even (key value pairs)")
         }
-        val model = HashMap<String, Any?>()
-        var i = 0
-        while (i < args.size) {
-            if (args[i] is String) {
-                model[args[i] as String] = args[i + 1]
-            } else {
-                throw IllegalArgumentException("Keys must be Strings")
-            }
-            i += 2
-        }
-        return model
+        return args.asSequence().chunked(2).associate { it[0] as String to it[1] }
     }
 }
