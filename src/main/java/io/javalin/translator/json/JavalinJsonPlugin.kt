cdc1f9d4d5901477bf2e5324051293ebed8b14fe
David <davidaase@hotmail.com>
2018-05-02 18:37:28 +0200

    put interfaces first

--- src/main/java/io/javalin/translator/json/JavalinJsonPlugin.kt
+++ src/main/java/io/javalin/translator/json/JavalinJsonPlugin.kt
@@ -8,31 +8,6 @@ package io.javalin.translator.json
 
 import io.javalin.core.util.Util
 
-object JavalinJsonPlugin {
-
-    @JvmStatic
-    var mapper: Any? = null
-
-    @JvmStatic
-    var jsonToObjectMapper = object : JsonToObjectMapper {
-        override fun <T> map(json: String, targetClass: Class<T>): T {
-            Util.ensureDependencyPresent("Jackson", "com.fasterxml.jackson.databind.ObjectMapper", "com.fasterxml.jackson.core/jackson-databind")
-            JavalinJacksonPlugin.objectMapper = JavalinJacksonPlugin.objectMapper ?: JavalinJacksonPlugin.createObjectMapper()
-            return JavalinJacksonPlugin.objectMapper!!.readValue(json, targetClass)
-        }
-    }
-
-    @JvmStatic
-    var objectToJsonMapper = object : ObjectToJsonMapper {
-        override fun map(obj: Any): String {
-            Util.ensureDependencyPresent("Jackson", "com.fasterxml.jackson.databind.ObjectMapper", "com.fasterxml.jackson.core/jackson-databind")
-            JavalinJacksonPlugin.objectMapper = JavalinJacksonPlugin.objectMapper ?: JavalinJacksonPlugin.createObjectMapper()
-            return JavalinJacksonPlugin.objectMapper!!.writeValueAsString(obj)
-        }
-    }
-
-}
-
 @FunctionalInterface
 interface JsonToObjectMapper {
     fun <T> map(json: String, targetClass: Class<T>): T
@@ -41,4 +16,29 @@ interface JsonToObjectMapper {
 @FunctionalInterface
 interface ObjectToJsonMapper {
     fun map(obj: Any): String
+}
+
+object JavalinJsonPlugin {
+
+    @JvmStatic
+    var mapper: Any? = null
+
+    @JvmStatic
+    var jsonToObjectMapper = object : JsonToObjectMapper {
+        override fun <T> map(json: String, targetClass: Class<T>): T { // this awkward implementation is for backwards compatibility
+            Util.ensureDependencyPresent("Jackson", "com.fasterxml.jackson.databind.ObjectMapper", "com.fasterxml.jackson.core/jackson-databind")
+            JavalinJacksonPlugin.objectMapper = JavalinJacksonPlugin.objectMapper ?: JavalinJacksonPlugin.createObjectMapper()
+            return JavalinJacksonPlugin.objectMapper!!.readValue(json, targetClass)
+        }
+    }
+
+    @JvmStatic
+    var objectToJsonMapper = object : ObjectToJsonMapper {
+        override fun map(obj: Any): String { // this awkward implementation is for backwards compatibility
+            Util.ensureDependencyPresent("Jackson", "com.fasterxml.jackson.databind.ObjectMapper", "com.fasterxml.jackson.core/jackson-databind")
+            JavalinJacksonPlugin.objectMapper = JavalinJacksonPlugin.objectMapper ?: JavalinJacksonPlugin.createObjectMapper()
+            return JavalinJacksonPlugin.objectMapper!!.writeValueAsString(obj)
+        }
+    }
+
 }
