61559a53ad30fe77492af5a51c6f6c1c7a560579
David Herges <dherges@users.noreply.github.com>
2018-05-10 11:11:32 +0200

    [extension] add Extension API (#208)
    * [extension] add Extension API
    
    * [extension] register class-based Extension by class name
    
    * [extension] allow anonymous and non-anonymous registrations
    
    * [extension] register app extensions by class key

--- src/main/java/io/javalin/Javalin.java
+++ src/main/java/io/javalin/Javalin.java
@@ -67,6 +67,8 @@ public class Javalin {
         throw new IllegalStateException("No access manager configured. Add an access manager using 'accessManager()'");
     };
 
+    private final Map<Class<?>, Extension> extensions = new HashMap<>();
+
     private Javalin() {
     }
 
@@ -740,4 +742,46 @@ public class Javalin {
         exceptionMapper.getExceptionMap().clear();
     }
 
+    // Extension
+
+    /**
+     * Registers an anonymous {@link Extension} with the Javalin application.
+     *
+     * @param extension You're free to implement the extension as a class or a lambda expression
+     * @return Self instance for fluent, method-chaining API
+     */
+    public Javalin extension(Extension extension) {
+        extension.register(this);
+
+        return this;
+    }
+
+    /**
+     * Registers an {@link Extension} with the Javalin application.
+     *
+     * @param extClazz The extension key
+     * @param extension You're free to implement the extension as a class or a lambda expression
+     * @return Self instance for fluent, method-chaining API
+     */
+    public Javalin extension(Class<?> extClazz, Extension extension) {
+        if (extClazz == null) {
+            throw new IllegalArgumentException("Extension key extClazz must not be null");
+        }
+        extension.register(this);
+        extensions.put(extClazz, extension);
+
+        return this;
+    }
+
+    /**
+     * Returns an {@link Extension} that was registered with the Javalin application.
+     *
+     * @param extClazz The class implementing the `Extension` interface
+     * @return An instance of `T` or null
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T extension(Class<T> extClazz) {
+
+        return (T) extensions.get(extClazz);
+    }
 }
