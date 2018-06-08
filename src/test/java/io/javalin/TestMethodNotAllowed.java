9f515d7e48c5c15ea7206b84043458d14a7422fc
David <davidaase@hotmail.com>
2018-06-08 21:05:51 +0200

    [test] Format 405 test code

--- src/test/java/io/javalin/TestMethodNotAllowed.java
+++ src/test/java/io/javalin/TestMethodNotAllowed.java
@@ -3,32 +3,30 @@ package io.javalin;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import javax.servlet.http.HttpServletResponse;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class TestMethodNotAllowed {
 
     private static final String EXPECTED_HTML_BODY = "\n"
-            + "                    <!DOCTYPE html>\n"
-            + "                    <html lang=\"en\">\n"
-            + "                        <head>\n"
-            + "                            <meta charset=\"UTF-8\">\n"
-            + "                            <title>Method Not Allowed</title>\n"
-            + "                        </head>\n"
-            + "                        <body>\n"
-            + "                            <h1>405 - Method Not Allowed</h1>\n"
-            + "                            <p>\n"
-            + "                                Available Methods: <strong>GET, PUT, DELETE</strong>\n"
-            + "                            </p>\n"
-            + "                        </body>\n"
-            + "                    </html>\n"
-            + "                ";
+        + "                    <!DOCTYPE html>\n"
+        + "                    <html lang=\"en\">\n"
+        + "                        <head>\n"
+        + "                            <meta charset=\"UTF-8\">\n"
+        + "                            <title>Method Not Allowed</title>\n"
+        + "                        </head>\n"
+        + "                        <body>\n"
+        + "                            <h1>405 - Method Not Allowed</h1>\n"
+        + "                            <p>\n"
+        + "                                Available Methods: <strong>GET, PUT, DELETE</strong>\n"
+        + "                            </p>\n"
+        + "                        </body>\n"
+        + "                    </html>\n"
+        + "                ";
     private static final String EXPECTED_JSON_BODY = "{\"availableMethods\":[\"GET\", \"PUT\", \"DELETE\"]}";
     private static Javalin app;
     private static String baseUrl;
@@ -36,9 +34,9 @@ public class TestMethodNotAllowed {
     @BeforeClass
     public static void setup() {
         app = Javalin.create()
-                     .port(0)
-                     .prefer405over404()
-                     .start();
+            .port(0)
+            .prefer405over404()
+            .start();
         baseUrl = "http://localhost:" + app.port();
 
         app.get("/test", ctx -> ctx.result("Hello world"));
