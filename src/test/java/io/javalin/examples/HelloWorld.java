cd8478d96c69e68d3b8c2d38bf288b7cf2cf7c4a
David <davidaase@hotmail.com>
2018-06-28 16:14:39 +0200

    Fix example

--- src/test/java/io/javalin/examples/HelloWorld.java
+++ src/test/java/io/javalin/examples/HelloWorld.java
@@ -11,7 +11,7 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().port(7070).enableDynamicEtags().start();
+        Javalin app = Javalin.create().port(7070).start();
         app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
