9dcc07227b7d68f3607b1ca7857cfbbc41200f47
David <davidaase@hotmail.com>
2018-06-28 17:12:48 +0200

    Test cleanup

--- src/test/java/io/javalin/TestDefaultContentType.java
+++ src/test/java/io/javalin/TestDefaultContentType.java
@@ -1,12 +1,9 @@
 package io.javalin;
 
-import com.mashape.unirest.http.HttpMethod;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.request.HttpRequestWithBody;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import io.javalin.core.util.Header;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,66 +11,45 @@ import static org.hamcrest.Matchers.containsString;
 
 public class TestDefaultContentType {
 
-    private static Javalin app;
-    private static String origin;
-
-    @BeforeClass
-    public static void setUp() {
-        app = Javalin.create()
-            .port(0)
-            .defaultContentType("application/json")
-            .start();
-        origin = "http://localhost:" + app.port();
-    }
-
-    @Test
-    public void test_allows_ISO_8859_1() throws Exception {
-        app.get("/test-iso-encoding", ctx -> {
-            ctx.response().setCharacterEncoding("iso-8859-1");
-            ctx.result("");
-        });
-        String contentType = Unirest.get(origin + "/test-iso-encoding").asString().getHeaders().getFirst(Header.CONTENT_TYPE);
-        assertThat(contentType, containsString("iso-8859-1"));
-    }
-
-    @Test
-    public void test_sets_default() throws Exception {
-        app.get("/test-default-encoding", ctx -> ctx.result(""));
-        String contentType = Unirest.get(origin + "/test-default-encoding").asString().getHeaders().getFirst(Header.CONTENT_TYPE);
-        assertThat(contentType, containsString("application/json"));
+    private HttpResponse<String> get(Javalin app, String path) throws UnirestException {
+        return Unirest.get("http://localhost:" + app.port() + path).asString();
     }
 
     @Test
     public void test_sane_defaults() throws Exception {
-        Javalin app = Javalin.create().start(0)
-            .get("/json", ctx -> ctx.json("白菜湯"))
-            .get("/html", ctx -> ctx.html("kålsuppe"))
-            .get("/text", ctx -> ctx.result("суп из капусты"));
-        HttpResponse<String> jsonResponse = Unirest.get("http://localhost:" + app.port() + "/json").asString();
-        HttpResponse<String> htmlResponse = Unirest.get("http://localhost:" + app.port() + "/html").asString();
-        HttpResponse<String> textResponse = Unirest.get("http://localhost:" + app.port() + "/text").asString();
-        assertThat(jsonResponse.getHeaders().getFirst(Header.CONTENT_TYPE), is("application/json"));
-        assertThat(htmlResponse.getHeaders().getFirst(Header.CONTENT_TYPE), is("text/html"));
-        assertThat(textResponse.getHeaders().getFirst(Header.CONTENT_TYPE), is("text/plain"));
-        assertThat(jsonResponse.getBody(), is("\"白菜湯\""));
-        assertThat(htmlResponse.getBody(), is("kålsuppe"));
-        assertThat(textResponse.getBody(), is("суп из капусты"));
+        Javalin app = Javalin.create().start(0);
+        app.get("/text", ctx -> ctx.result("суп из капусты"));
+        app.get("/json", ctx -> ctx.json("白菜湯"));
+        app.get("/html", ctx -> ctx.html("kålsuppe"));
+        assertThat(get(app, "/text").getHeaders().getFirst(Header.CONTENT_TYPE), is("text/plain"));
+        assertThat(get(app, "/json").getHeaders().getFirst(Header.CONTENT_TYPE), is("application/json"));
+        assertThat(get(app, "/html").getHeaders().getFirst(Header.CONTENT_TYPE), is("text/html"));
+        assertThat(get(app, "/text").getBody(), is("суп из капусты"));
+        assertThat(get(app, "/json").getBody(), is("\"白菜湯\""));
+        assertThat(get(app, "/html").getBody(), is("kålsuppe"));
+        app.stop();
+    }
+
+    @Test
+    public void test_sets_default() throws Exception {
+        Javalin app = Javalin.create().defaultContentType("application/json").start(0);
+        app.get("/default", ctx -> ctx.result("not json"));
+        assertThat(get(app, "/default").getHeaders().getFirst(Header.CONTENT_TYPE), containsString("application/json"));
+        app.stop();
     }
 
     @Test
     public void test_allows_overrides() throws Exception {
-        app.get("/test-override-encoding", ctx -> {
+        Javalin app = Javalin.create().defaultContentType("application/json").start(0);
+        app.get("/override", ctx -> {
             ctx.response().setCharacterEncoding("utf-8");
             ctx.response().setContentType("text/html");
-            ctx.result("");
+            ctx.result("mmm");
         });
-        String contentType = Unirest.get(origin + "/test-override-encoding").asString().getHeaders().getFirst(Header.CONTENT_TYPE);
+        String contentType = get(app, "/override").getHeaders().getFirst(Header.CONTENT_TYPE);
         assertThat(contentType, containsString("utf-8"));
         assertThat(contentType, containsString("text/html"));
-    }
-
-    @AfterClass
-    public static void tearDown() {
         app.stop();
     }
+
 }
