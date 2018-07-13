e385abd1d9a0a2d6b03844eb910fb3ae2d7d2747
Hexagon <hexagondev@gmail.com>
2018-07-12 21:17:03 -0300

    [core] Removed the default status code from the HaltException

--- src/main/java/io/javalin/HaltException.kt
+++ src/main/java/io/javalin/HaltException.kt
@@ -10,12 +10,9 @@ package io.javalin
  * Exception which is handled before all other exceptions. Can be used to short-circuit the request cycle.
  * @see <a href="https://javalin.io/documentation#exception-mapping">HaltException in docs</a>
  */
-class HaltException(var statusCode: Int = 200, var body: String = "") : RuntimeException() {
-    constructor(statusCode: Int) : this() {
-        this.statusCode = statusCode
-    }
+class HaltException(var statusCode: Int, var body: String) : RuntimeException() {
 
-    constructor(body: String) : this() {
-        this.body = body
+    constructor(statusCode: Int) : this(statusCode, "") {
+        this.statusCode = statusCode
     }
 }
