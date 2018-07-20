63c3c8f11bcdf68b42ba5d0276022effde2049be
Joel Gerber <joel@grrbrr.ca>
2018-07-20 17:26:07 -0400

    Fix graceful shutdown test
    It was failing, at least I thought it was. I think I only thought it
    was, because it logged an exception, but the test didn't actually fail
    when I re-ran it today. This resorts to using a future, that we block
    wait for after the shutdown has been triggered. This was I can get the
    exception thrown in the same thread as JUnit, and also more properly
    format the test as when, then, assert.
    
    I also took the time to rewrite the test a bit to fall somewhere
    between my natural style and @tipsy's preferred style.

--- src/test/java/io/javalin/TestGracefulShutdown.java
+++ src/test/java/io/javalin/TestGracefulShutdown.java
@@ -8,66 +8,45 @@ package io.javalin;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.async.Callback;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.Future;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 public class TestGracefulShutdown {
     private static final int LONG_WAIT_TIME_IN_MSECS = 500;
     private static final int CONNECT_WAIT_TIME_IN_MSEC = 30;
 
-    private Javalin app;
-
-    @Before
-    public void configureJavalinBeforeEachTest() {
-        app = Javalin.create().disableStartupBanner().port(0);
-        app.get("/immediate-response", context -> context.status(200));
-        app.get("/delayed-response", context -> Thread.sleep(LONG_WAIT_TIME_IN_MSECS));
-        app.start();
-    }
-
     @Test
     public void long_running_query_does_not_abort_during_graceful_shutdown() throws Exception {
-        waitForServletToLoad();
-        validateLongRunningRequestCompletes();
-        waitForSocketToConnect();
-        performShutdown();
+        Javalin app = launchedJavalinApplication();
+        waitForJavalinServletToLoad(app);
+
+        Future<HttpResponse<String>> runningRequest = performLongRunningRequest(app);
+        app.stop(); // request has not completed yet
+
+        assertThat(runningRequest.get().getStatus(), equalTo(200));
     }
 
-    private void waitForServletToLoad() throws Exception {
-        String baseUri = String.format("http://localhost:%d", app.port());
-        Unirest.get(baseUri + "/immediate-response").asString();
+    private Javalin launchedJavalinApplication() {
+        return Javalin.create().disableStartupBanner().port(0)
+                .get("/immediate-response", context -> context.status(200))
+                .get("/delayed-response", context -> Thread.sleep(LONG_WAIT_TIME_IN_MSECS))
+                .start();
     }
 
-    private void validateLongRunningRequestCompletes() {
-        String baseUri = String.format("http://localhost:%d", app.port());
-        Unirest.get(baseUri + "/delayed-response").asStringAsync(new AsyncResponseHandler());
+    private void waitForJavalinServletToLoad(Javalin app) throws Exception {
+        String requestUri = String.format("http://localhost:%d/%s", app.port(), "immediate-response");
+        Unirest.get(requestUri).asString();
     }
 
-    private void waitForSocketToConnect() throws Exception {
+    private Future<HttpResponse<String>> performLongRunningRequest(Javalin app) throws Exception {
+        String requestUri = String.format("http://localhost:%d/%s", app.port(), "delayed-response");
+        Future<HttpResponse<String>> responseFuture = Unirest.get(requestUri).asStringAsync();
         Thread.sleep(CONNECT_WAIT_TIME_IN_MSEC);
-    }
 
-    private void performShutdown() {
-        app.stop();
-    }
-
-    static class AsyncResponseHandler implements Callback<String> {
-        public void completed(HttpResponse<String> response) {
-            assertThat(response.getStatus(), equalTo(200));
-        }
-
-        public void failed(UnirestException exception) {
-            fail("request failed: " + exception.getMessage());
-        }
-
-        public void cancelled() {
-            fail("request was cancelled");
-        }
+        return responseFuture;
     }
 }
