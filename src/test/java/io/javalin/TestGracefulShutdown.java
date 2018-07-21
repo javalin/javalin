1f2ebecd6df80e451853c8fd020f3473daf42387
Joel Gerber <joel@grrbrr.ca>
2018-07-21 07:39:16 -0400

    Add tests for expected behaviour when Jetty is manually configured
    This adds a test that a request completes when Jetty is manually
    configured the same as production code (with a statistics handler
    configured) and that a request fails when Jetty is configured without
    a statistics handler configured.

--- src/test/java/io/javalin/TestGracefulShutdown.java
+++ src/test/java/io/javalin/TestGracefulShutdown.java
@@ -8,8 +8,11 @@ package io.javalin;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.StatisticsHandler;
 import org.junit.Test;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -20,8 +23,8 @@ public class TestGracefulShutdown {
     private static final int CONNECT_WAIT_TIME_IN_MSEC = 30;
 
     @Test
-    public void long_running_query_does_not_abort_during_graceful_shutdown() throws Exception {
-        Javalin app = launchedJavalinApplication();
+    public void long_running_query_completes_during_shutdown_when_jetty_is_auto_configured() throws Exception {
+        Javalin app = sharedJavalinConfiguration().start();
         waitForJavalinServletToLoad(app);
 
         Future<HttpResponse<String>> runningRequest = performLongRunningRequest(app);
@@ -30,11 +33,36 @@ public class TestGracefulShutdown {
         assertThat(runningRequest.get().getStatus(), equalTo(200));
     }
 
-    private Javalin launchedJavalinApplication() {
+    @Test
+    public void long_running_query_completes_during_shutdown_when_jetty_is_manually_configured_with_statistics_handler() throws Exception {
+        Javalin app = sharedJavalinConfiguration().server(() -> {
+            Server server = new Server();
+            server.insertHandler(new StatisticsHandler());
+            return server;
+        }).start();
+        waitForJavalinServletToLoad(app);
+
+        Future<HttpResponse<String>> runningRequest = performLongRunningRequest(app);
+        app.stop(); // request has not completed yet
+
+        assertThat(runningRequest.get().getStatus(), equalTo(200));
+    }
+
+    @Test(expected = ExecutionException.class)
+    public void long_running_query_aborts_during_shutdown_when_jetty_is_manually_configured_without_statistics_handler() throws Exception {
+        Javalin app = sharedJavalinConfiguration().server(Server::new).start();
+        waitForJavalinServletToLoad(app);
+
+        Future<HttpResponse<String>> runningRequest = performLongRunningRequest(app);
+        app.stop(); // request has not completed yet
+
+        runningRequest.get();
+    }
+
+    private Javalin sharedJavalinConfiguration() {
         return Javalin.create().disableStartupBanner().port(0)
                 .get("/immediate-response", context -> context.status(200))
-                .get("/delayed-response", context -> Thread.sleep(LONG_WAIT_TIME_IN_MSECS))
-                .start();
+                .get("/delayed-response", context -> Thread.sleep(LONG_WAIT_TIME_IN_MSECS));
     }
 
     private void waitForJavalinServletToLoad(Javalin app) throws Exception {
