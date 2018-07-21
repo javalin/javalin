12ae16d56fdc519ac256ec3fc961af0b20e8a4b3
David <davidaase@hotmail.com>
2018-07-21 17:03:41 +0200

    [test] Run graceful tests in fixed order

--- src/test/java/io/javalin/TestGracefulShutdown.java
+++ src/test/java/io/javalin/TestGracefulShutdown.java
@@ -2,79 +2,74 @@
  * Javalin - https://javalin.io
  * Copyright 2017 David Åse
  * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ *
+ * @author: Jitsusama
  */
 
 package io.javalin;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.StatisticsHandler;
-import org.junit.Test;
-
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.StatisticsHandler;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestGracefulShutdown {
+
     private static final int LONG_WAIT_TIME_IN_MSECS = 500;
     private static final int CONNECT_WAIT_TIME_IN_MSECS = 30;
 
     @Test
-    public void long_running_request_completes_during_shutdown_when_jetty_is_auto_configured() throws Exception {
+    public void t1_shutdown_is_graceful_with_default_config() throws Exception {
         Javalin app = sharedJavalinConfiguration().start();
-        waitForJavalinServletToLoad(app);
-
-        Future<HttpResponse<String>> runningRequest = performLongRunningRequest(app);
-        app.stop(); // request has not completed yet
-
-        assertThat(runningRequest.get().getStatus(), equalTo(200));
+        testIfShutdownIsGraceful(app);
     }
 
     @Test
-    public void long_running_request_completes_during_shutdown_when_jetty_is_manually_configured_with_statistics_handler() throws Exception {
+    public void t2_shutdown_is_graceful_when_custom_nserver_has_statisticshandler() throws Exception {
         Javalin app = sharedJavalinConfiguration().server(() -> {
             Server server = new Server();
             server.insertHandler(new StatisticsHandler());
             return server;
         }).start();
-        waitForJavalinServletToLoad(app);
-
-        Future<HttpResponse<String>> runningRequest = performLongRunningRequest(app);
-        app.stop(); // request has not completed yet
-
-        assertThat(runningRequest.get().getStatus(), equalTo(200));
+        testIfShutdownIsGraceful(app);
     }
 
     @Test(expected = ExecutionException.class)
-    public void long_running_request_aborts_during_shutdown_when_jetty_is_manually_configured_without_statistics_handler() throws Exception {
+    public void t3_shutdown_is_not_graceful_when_custom_server_has_no_statisticshandler() throws Exception {
         Javalin app = sharedJavalinConfiguration().server(Server::new).start();
-        waitForJavalinServletToLoad(app);
+        testIfShutdownIsGraceful(app); // should throw exception
+    }
 
-        Future<HttpResponse<String>> runningRequest = performLongRunningRequest(app);
+    private void testIfShutdownIsGraceful(Javalin app) throws Exception {
+        performBlockingRequest(app);
+        Future<HttpResponse<String>> asyncResponse = performAsyncRequest(app);
         app.stop(); // request has not completed yet
-
-        runningRequest.get();
+        assertThat(asyncResponse.get().getStatus(), equalTo(200));
     }
 
     private Javalin sharedJavalinConfiguration() {
         return Javalin.create().disableStartupBanner().port(0)
-                .get("/immediate-response", context -> context.status(200))
-                .get("/delayed-response", context -> Thread.sleep(LONG_WAIT_TIME_IN_MSECS));
+            .get("/immediate-response", context -> context.status(200))
+            .get("/delayed-response", context -> Thread.sleep(LONG_WAIT_TIME_IN_MSECS));
     }
 
-    private void waitForJavalinServletToLoad(Javalin app) throws Exception {
+    private void performBlockingRequest(Javalin app) throws Exception {
         String requestUri = String.format("http://localhost:%d/%s", app.port(), "immediate-response");
         Unirest.get(requestUri).asString();
     }
 
-    private Future<HttpResponse<String>> performLongRunningRequest(Javalin app) throws Exception {
+    private Future<HttpResponse<String>> performAsyncRequest(Javalin app) throws Exception {
         String requestUri = String.format("http://localhost:%d/%s", app.port(), "delayed-response");
         Future<HttpResponse<String>> responseFuture = Unirest.get(requestUri).asStringAsync();
         Thread.sleep(CONNECT_WAIT_TIME_IN_MSECS);
-
         return responseFuture;
     }
 }
