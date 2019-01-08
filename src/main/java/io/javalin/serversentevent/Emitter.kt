9b861a344864d73aad654b2365d5153810c36158
Agustí Becerra Milà <contact@agustibm.com>
2019-01-08 09:37:59 +0100

    Change CRLF to CR

--- src/main/java/io/javalin/serversentevent/Emitter.kt
+++ src/main/java/io/javalin/serversentevent/Emitter.kt
@@ -11,7 +11,7 @@ import java.io.BufferedReader
 class Emitter(private var asyncContext: AsyncContext) {
     private var close: Boolean = false
     private var output: ServletOutputStream? = null
-    private val CRLF = "\r\n"
+    private val CR = "\n"
 
     init {
         try {
@@ -24,10 +24,10 @@ class Emitter(private var asyncContext: AsyncContext) {
     fun event(event: String, data: String) {
         synchronized(this) {
             try {
-                output?.println("event: $event$CRLF")
+                output?.println("event: $event$CR")
                 val reader = BufferedReader(StringReader(data))
-                reader.lineSequence().forEach { line -> output?.println("data: $line$CRLF") }
-                output?.println("$CRLF")
+                reader.lineSequence().forEach { line -> output?.println("data: $line$CR") }
+                output?.println("$CR$CR")
                 asyncContext.response.flushBuffer()
             } catch (e: IOException) {
                 close = true
@@ -40,9 +40,9 @@ class Emitter(private var asyncContext: AsyncContext) {
     fun event(id: Int, event: String, data: String) {
         synchronized(this) {
             try {
-                output?.println("id: $id$CRLF")
-                output?.println("event: $event$CRLF")
-                output?.println("data: $data$CRLF")
+                output?.println("id: $id$CR")
+                output?.println("event: $event$CR")
+                output?.println("data: $data$CR")
                 asyncContext.response.flushBuffer()
             } catch (e: IOException) {
                 close = true
