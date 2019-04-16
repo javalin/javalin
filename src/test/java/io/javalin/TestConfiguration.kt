7c881f53c234b16112db8240d29e057cd5cbadc1
David <davidaase@hotmail.com>
2019-04-16 18:13:13 +0200

    [test] Fix config test

--- src/test/java/io/javalin/TestConfiguration.kt
+++ src/test/java/io/javalin/TestConfiguration.kt
@@ -6,7 +6,9 @@
 
 package io.javalin
 
+import io.javalin.security.SecurityUtil.roles
 import io.javalin.staticfiles.Location
+import org.assertj.core.api.Assertions.assertThat
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.session.SessionHandler
 import org.junit.Test
@@ -16,28 +18,41 @@ class TestConfiguration {
     @Test
     fun `test all config options`() {
         val app = Javalin.create {
+            // JavalinServlet
             it.addSinglePageRoot("/", "/public/html.html")
             it.addSinglePageRoot("/", "src/test/resources/public/html.html", Location.EXTERNAL)
             it.addStaticFiles("/public")
             it.addStaticFiles("src/test/resources/public", Location.EXTERNAL)
+            it.asyncRequestTimeout = 10_000L
+            it.autogenerateEtags = true
             it.contextPath = "/"
             it.defaultContentType = "text/plain"
             it.dynamicGzip = true
-            it.enableWebjars()
+            it.enableCorsForAllOrigins()
             it.enableCorsForOrigin("*", "my-origin")
+            it.enableDevLogging()
+            it.enableRouteOverview("/test")
+            it.enableRouteOverview("/test", roles())
+            it.enableWebjars()
+            it.enforceSsl = true
             it.prefer405over404 = false
-            it.autogenerateEtags = true
             it.requestCacheSize = 8192L
             it.requestLogger { ctx, executionTimeMs -> }
-            it.accessManager { handler, ctx, permittedRoles -> }
             it.sessionHandler { SessionHandler() }
+            // WsServlet
             it.wsContextPath = "/"
             it.wsFactoryConfig { }
             it.wsLogger { }
+            // Server
             it.server {
                 Server()
             }
-        }
+            // Misc
+            it.accessManager { handler, ctx, permittedRoles -> }
+            it.showJavalinBanner = false
+        }.start()
+        assertThat(app.server.started).isTrue()
+        app.stop()
     }
 
 }
