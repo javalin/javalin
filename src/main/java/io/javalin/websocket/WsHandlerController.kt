0f5b4fa3e0a6d7382ba7434d8cf02448ee980346
David <davidaase@hotmail.com>
2019-04-18 15:41:42 +0200

    [websocket] Create helper functions in WsHandlerController

--- src/main/java/io/javalin/websocket/WsHandlerController.kt
+++ src/main/java/io/javalin/websocket/WsHandlerController.kt
@@ -17,143 +17,71 @@ import java.util.concurrent.ConcurrentHashMap
  * matching custom handlers.
  */
 @WebSocket
-class WsHandlerController(private val matcher: WsPathMatcher,
-                          private val exceptionMapper: WsExceptionMapper,
-                          private val wsLogger: WsHandler?) {
+class WsHandlerController(val matcher: WsPathMatcher, val exceptionMapper: WsExceptionMapper, val wsLogger: WsHandler?) {
 
     private val sessionIds = ConcurrentHashMap<Session, String>()
 
     @OnWebSocketConnect
     fun onConnect(session: Session) {
-        // Associate a unique ID with this new session
-        val sessionId = UUID.randomUUID().toString()
-        sessionIds[session] = sessionId
-
-        val requestUri = session.uriNoContextPath()
-        val ctx = WsConnectContext(sessionId, session)
-
-        try {
-            // Invoke before handlers (if any)
-            matcher.findBeforeHandlerEntries(requestUri).forEach { it.handler.connectHandler?.handleConnect(ctx) }
-
-            // Invoke endpoint handler (can't be null, otherwise the ws servlet would not have invoked the controller)
-            matcher.findEndpointHandlerEntry(requestUri)!!.let { it.handler.connectHandler?.handleConnect(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
-        // Invoke after handlers (if any)
-        try {
-            matcher.findAfterHandlerEntries(requestUri).forEach { it.handler.connectHandler?.handleConnect(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
+        sessionIds[session] = UUID.randomUUID().toString() // associate a unique ID with this new session
+        val ctx = WsConnectContext(sessionIds[session]!!, session)
+        tryBeforeAndEndpointHandlers(ctx) { it.handler.connectHandler?.handleConnect(ctx) }
+        tryAfterHandlers(ctx) { it.handler.connectHandler?.handleConnect(ctx) }
         wsLogger?.connectHandler?.handleConnect(ctx)
     }
 
     @OnWebSocketMessage
     fun onMessage(session: Session, message: String) {
-        val requestUri = session.uriNoContextPath()
         val ctx = WsMessageContext(sessionIds[session]!!, session, message)
-
-        try {
-            // Invoke before handlers (if any)
-            matcher.findBeforeHandlerEntries(requestUri).forEach { it.handler.messageHandler?.handleMessage(ctx) }
-
-            // Invoke endpoint handler (can't be null, otherwise the ws servlet would not have invoked the controller)
-            matcher.findEndpointHandlerEntry(requestUri)!!.let { it.handler.messageHandler?.handleMessage(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
-        // Invoke after handlers (if any)
-        try {
-            matcher.findAfterHandlerEntries(requestUri).forEach { it.handler.messageHandler?.handleMessage(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
+        tryBeforeAndEndpointHandlers(ctx) { it.handler.messageHandler?.handleMessage(ctx) }
+        tryAfterHandlers(ctx) { it.handler.messageHandler?.handleMessage(ctx) }
         wsLogger?.messageHandler?.handleMessage(ctx)
     }
 
     @OnWebSocketMessage
     fun onMessage(session: Session, buffer: ByteArray, offset: Int, length: Int) {
-        val requestUri = session.uriNoContextPath()
         val ctx = WsBinaryMessageContext(sessionIds[session]!!, session, buffer.toTypedArray(), offset, length)
-
-        try {
-            // Invoke before handlers (if any)
-            matcher.findBeforeHandlerEntries(requestUri).forEach { it.handler.binaryMessageHandler?.handleBinaryMessage(ctx) }
-
-            // Invoke endpoint handler (can't be null, otherwise the ws servlet would not have invoked the controller)
-            matcher.findEndpointHandlerEntry(requestUri)!!.let { it.handler.binaryMessageHandler?.handleBinaryMessage(ctx) }
-        } catch (e: java.lang.Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
-        // Invoke after handlers (if any)
-        try {
-            matcher.findAfterHandlerEntries(requestUri).forEach { it.handler.binaryMessageHandler?.handleBinaryMessage(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
+        tryBeforeAndEndpointHandlers(ctx) { it.handler.binaryMessageHandler?.handleBinaryMessage(ctx) }
+        tryAfterHandlers(ctx) { it.handler.binaryMessageHandler?.handleBinaryMessage(ctx) }
         wsLogger?.binaryMessageHandler?.handleBinaryMessage(ctx)
     }
 
     @OnWebSocketClose
     fun onClose(session: Session, statusCode: Int, reason: String?) {
-        val requestUri = session.uriNoContextPath()
         val ctx = WsCloseContext(sessionIds[session]!!, session, statusCode, reason)
-
-        try {
-            // Invoke before handlers (if any)
-            matcher.findBeforeHandlerEntries(requestUri).forEach { it.handler.closeHandler?.handleClose(ctx) }
-
-            // Invoke endpoint handler (can't be null, otherwise the ws servlet would not have invoked the controller)
-            matcher.findEndpointHandlerEntry(requestUri)!!.let { it.handler.closeHandler?.handleClose(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
-        // Invoke after handlers (if any)
-        try {
-            matcher.findAfterHandlerEntries(requestUri).forEach { it.handler.closeHandler?.handleClose(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
+        tryBeforeAndEndpointHandlers(ctx) { it.handler.closeHandler?.handleClose(ctx) }
+        tryAfterHandlers(ctx) { it.handler.closeHandler?.handleClose(ctx) }
         wsLogger?.closeHandler?.handleClose(ctx)
-
-        // The socket has been closed, we no longer need to keep track of the session ID
         sessionIds.remove(session)
     }
 
     @OnWebSocketError
     fun onError(session: Session, throwable: Throwable?) {
-        val requestUri = session.uriNoContextPath()
         val ctx = WsErrorContext(sessionIds[session]!!, session, throwable)
-
-        try {
-            // Invoke before handlers (if any)
-            matcher.findBeforeHandlerEntries(requestUri).forEach { it.handler.errorHandler?.handleError(ctx) }
-
-            // Invoke endpoint handler (can't be null, otherwise the ws servlet would not have invoked the controller)
-            matcher.findEndpointHandlerEntry(requestUri)!!.let { it.handler.errorHandler?.handleError(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
-        // Invoke after handlers (if any)
-        try {
-            matcher.findAfterHandlerEntries(requestUri).forEach { it.handler.errorHandler?.handleError(ctx) }
-        } catch (e: Exception) {
-            exceptionMapper.handle(e, ctx)
-        }
-
+        tryBeforeAndEndpointHandlers(ctx) { it.handler.errorHandler?.handleError(ctx) }
+        tryAfterHandlers(ctx) { it.handler.errorHandler?.handleError(ctx) }
         wsLogger?.errorHandler?.handleError(ctx)
     }
+
+    private fun tryBeforeAndEndpointHandlers(ctx: WsContext, handle: (WsEntry) -> Unit) {
+        val requestUri = ctx.session.uriNoContextPath()
+        try {
+            matcher.findBeforeHandlerEntries(requestUri).forEach { handle.invoke(it) }
+            matcher.findEndpointHandlerEntry(requestUri)!!.let { handle.invoke(it) } // never null, 404 is handled in front
+        } catch (e: Exception) {
+            exceptionMapper.handle(e, ctx)
+        }
+    }
+
+    private fun tryAfterHandlers(ctx: WsContext, handle: (WsEntry) -> Unit) {
+        val requestUri = ctx.session.uriNoContextPath()
+        try {
+            matcher.findAfterHandlerEntries(requestUri).forEach { handle.invoke(it) }
+        } catch (e: Exception) {
+            exceptionMapper.handle(e, ctx)
+        }
+    }
+
 }
 
 private fun Session.uriNoContextPath() = this.upgradeRequest.requestURI.path.removePrefix((this.upgradeRequest as ServletUpgradeRequest).httpServletRequest.contextPath)
