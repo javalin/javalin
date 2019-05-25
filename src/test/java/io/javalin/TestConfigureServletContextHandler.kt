292fdbe1b940122c6bd0fe980a1b3919425f3a66
Marco Perazzo <marco.perazzo85@gmail.com>
2019-05-25 14:08:40 +0200

    Test showing that added filter is not called

--- src/test/java/io/javalin/TestConfigureServletContextHandler.kt
+++ src/test/java/io/javalin/TestConfigureServletContextHandler.kt
@@ -8,10 +8,12 @@
 package io.javalin
 
 import io.javalin.misc.HttpUtil
+import org.eclipse.jetty.servlet.FilterHolder
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import javax.servlet.ServletContextEvent
-import javax.servlet.ServletContextListener
+import java.util.*
+import javax.servlet.*
 
 class TestConfigureServletContextHandler {
 
@@ -37,6 +39,46 @@ class TestConfigureServletContextHandler {
         http.htmlGet("/");
 
         assertTrue(listener.called)
+    }
+
+    @Test
+    fun `Adding a filter to the ServletContextHandler works`() {
+        val filter = object :Filter {
+            var initialized = false
+            var called = false
+            var destroyed = false
+            override fun init(config: FilterConfig?) {
+                initialized = true
+            }
+            override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+                called = true
+                chain?.doFilter(request, response)
+            }
+            override fun destroy() {
+                destroyed = true
+            }
+        }
+
+        val app = Javalin.create {
+            it.configureServletContextHandler { handler ->
+                handler.addFilter(FilterHolder(filter), "/*", EnumSet.allOf(DispatcherType::class.java))
+            }
+        }.start()
+
+        app.get("/test") {
+            it.result("Test")
+        }
+
+        val http = HttpUtil(app)
+        var response = http.get("/test");
+
+        assertEquals("Test", response.body)
+
+        app.stop()
+
+        assertTrue(filter.initialized)
+        assertTrue(filter.called)
+        assertTrue(filter.destroyed)
     }
 
 }
