dbabd921c39f7228185fc3124f37d5047a4edcfc
mju <mju@dmi.dk>
2019-08-26 09:27:11 +0200

    JacksonToYamlMapper

--- src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
+++ src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
@@ -3,6 +3,7 @@ package io.javalin.plugin.openapi.jackson
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.plugin.json.ToJsonMapper
+import io.swagger.v3.core.util.Yaml
 
 /**
  * Default jackson mapper for creating the object api schema json.
@@ -10,6 +11,13 @@ import io.javalin.plugin.json.ToJsonMapper
  */
 object JacksonToJsonMapper : ToJsonMapper {
     val objectMapper by lazy { jacksonObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL) }
+
+    override fun map(obj: Any): String = objectMapper.writeValueAsString(obj)
+}
+
+
+object JacksonToYamlMapper : ToJsonMapper {
+    val objectMapper by lazy { Yaml.pretty() }
 
     override fun map(obj: Any): String = objectMapper.writeValueAsString(obj)
 }
