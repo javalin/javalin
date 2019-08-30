d2ce2416ec3f9e5521777021940d2378815c3db0
Maximilian Hippler <maximilian@hiiw.de>
2019-08-30 13:17:44 +0200

    Use predefined toStringSerializer from jackson

--- src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
+++ src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
@@ -1,10 +1,8 @@
 package io.javalin.plugin.openapi.jackson
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.plugin.json.ToJsonMapper
 import io.swagger.v3.oas.models.security.SecurityScheme
@@ -18,15 +16,9 @@ object JacksonToJsonMapper : ToJsonMapper {
         jacksonObjectMapper()
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
                 .registerModule(SimpleModule()
-                        .addSerializer(SecurityScheme.Type::class.java, EnumToStringSerializer())
-                        .addSerializer(SecurityScheme.In::class.java, EnumToStringSerializer()))
+                        .addSerializer(SecurityScheme.Type::class.java, ToStringSerializer())
+                        .addSerializer(SecurityScheme.In::class.java, ToStringSerializer()))
     }
 
     override fun map(obj: Any): String = objectMapper.writeValueAsString(obj)
-}
-
-class EnumToStringSerializer<E : Enum<E>> : StdSerializer<E>(null as Class<E>?) {
-    override fun serialize(value: E, jgen: JsonGenerator, provider: SerializerProvider) {
-        jgen.writeString(value.toString())
-    }
 }
