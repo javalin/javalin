423c28ad648915724f0e7c29e19bc199db331826
maxemann96 <maxemann96@users.noreply.github.com>
2019-09-01 09:44:49 +0200

    [openapi] Fixed security scheme to specification (#724)
    * Fixed security scheme to specification
    
    * Simplified custom enum serializer
    
    * Use predefined toStringSerializer from jackson

--- src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
+++ src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
@@ -1,15 +1,24 @@
 package io.javalin.plugin.openapi.jackson
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.plugin.json.ToJsonMapper
+import io.swagger.v3.oas.models.security.SecurityScheme
 
 /**
  * Default jackson mapper for creating the object api schema json.
  * This enables some of the options that are required to work with the uis.
  */
 object JacksonToJsonMapper : ToJsonMapper {
-    val objectMapper by lazy { jacksonObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL) }
+    val objectMapper by lazy {
+        jacksonObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .registerModule(SimpleModule()
+                        .addSerializer(SecurityScheme.Type::class.java, ToStringSerializer())
+                        .addSerializer(SecurityScheme.In::class.java, ToStringSerializer()))
+    }
 
     override fun map(obj: Any): String = objectMapper.writeValueAsString(obj)
 }
