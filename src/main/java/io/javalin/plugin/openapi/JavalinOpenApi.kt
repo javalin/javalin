fea9a45083f32abdc7633ba45e83fa51c6dfdd96
Maximilian Hippler <maximilian@hiiw.de>
2019-09-06 20:44:48 +0200

    Add warning if schema is invalid

--- src/main/java/io/javalin/plugin/openapi/JavalinOpenApi.kt
+++ src/main/java/io/javalin/plugin/openapi/JavalinOpenApi.kt
@@ -7,9 +7,12 @@ import io.javalin.plugin.openapi.dsl.applyMetaInfoList
 import io.javalin.plugin.openapi.dsl.updateComponents
 import io.javalin.plugin.openapi.dsl.updatePaths
 import io.javalin.plugin.openapi.jackson.JacksonModelConverterFactory
+import io.swagger.util.Yaml
 import io.swagger.v3.core.converter.ModelConverter
 import io.swagger.v3.core.converter.ModelConverters
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.parser.OpenAPIV3Parser
+import org.slf4j.LoggerFactory
 
 class CreateSchemaOptions(
         val handlerMetaInfoList: List<HandlerMetaInfo>,
@@ -42,7 +45,7 @@ object JavalinOpenApi {
     fun createSchema(options: CreateSchemaOptions): OpenAPI {
         val baseConfiguration = options.initialConfigurationCreator.create()
         val modelConverter = options.modelConverterFactory.create()
-        return runWithModelConverter(modelConverter) {
+        val schema = runWithModelConverter(modelConverter) {
             baseConfiguration.apply {
                 updateComponents {
                     applyMetaInfoList(options.handlerMetaInfoList, options)
@@ -52,6 +55,18 @@ object JavalinOpenApi {
                 }
             }
         }
+
+        val parsedSchema = OpenAPIV3Parser().readContents(Yaml.mapper().writeValueAsString(schema))
+
+        if(parsedSchema.messages.isNotEmpty()){
+            LoggerFactory.getLogger(JavalinOpenApi::javaClass.get()).warn("The generated OpenApi specification is not valid")
+
+            parsedSchema.messages.forEach {
+                LoggerFactory.getLogger(JavalinOpenApi::javaClass.get()).warn(it)
+            }
+        }
+
+        return schema
     }
 }
 
