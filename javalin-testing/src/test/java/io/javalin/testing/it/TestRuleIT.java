eccc6dc3820d414d724ccd618c6b4ef766669d4e
Alexey Gavrilov <agavrilov@telenordigital.com>
2019-10-07 20:48:32 +0200

    Sync with master
    - Sync with master
    - Remove useless default constructor
    - Make sure CI runs java integration test (mvn verify)

--- javalin-testing/src/test/java/io/javalin/testing/it/TestRuleIT.java
+++ javalin-testing/src/test/java/io/javalin/testing/it/TestRuleIT.java
@@ -6,15 +6,12 @@ import org.junit.Rule;
 import org.junit.Test;
 
 public class TestRuleIT {
-    @Rule
-    public JavalinRule javalinRule1 = new JavalinRule((it) -> it.showJavalinBanner = false);
 
     @Rule
-    public JavalinRule javalinRule2 = new JavalinRule();
+    public JavalinRule javalinRule1 = new JavalinRule((it) -> it.showJavalinBanner = false);
 
     @Test
     public void checkJavalinRuleWorks() {
         Assert.assertEquals(404, javalinRule1.getHttp().get("/not-found").getStatus());
-        Assert.assertEquals(404, javalinRule2.getHttp().get("/not-found").getStatus());
     }
 }
--- javalin/src/test/java/io/javalin/testing/JavalinRule.java
+++ javalin/src/test/java/io/javalin/testing/JavalinRule.java
@@ -2,11 +2,11 @@ package io.javalin.testing;
 
 import io.javalin.Javalin;
 import io.javalin.core.JavalinConfig;
+import java.util.function.Consumer;
 import org.junit.rules.ExternalResource;
 
-import java.util.function.Consumer;
-
 public class JavalinRule extends ExternalResource {
+
     private final Consumer<JavalinConfig> config;
 
     private Javalin javalin;
@@ -14,10 +14,6 @@ public class JavalinRule extends ExternalResource {
 
     public JavalinRule(Consumer<JavalinConfig> config) {
         this.config = config;
-    }
-
-    public JavalinRule() {
-        this.config = JavalinConfig.noopConfig;
     }
 
     public Javalin getJavalin() {
