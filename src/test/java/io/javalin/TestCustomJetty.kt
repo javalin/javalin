8d70f0fae327b2b5bbd5c76f1cf6c74f2d83ac31
David <davidaase@hotmail.com>
2020-03-20 16:52:06 +0100

    Remove unused import

--- src/test/java/io/javalin/TestCustomJetty.kt
+++ src/test/java/io/javalin/TestCustomJetty.kt
@@ -15,7 +15,6 @@ import org.eclipse.jetty.server.RequestLog
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.handler.ContextHandlerCollection
 import org.eclipse.jetty.server.handler.HandlerCollection
-import org.eclipse.jetty.server.handler.HandlerList
 import org.eclipse.jetty.server.handler.HandlerWrapper
 import org.eclipse.jetty.server.handler.RequestLogHandler
 import org.eclipse.jetty.server.handler.StatisticsHandler
