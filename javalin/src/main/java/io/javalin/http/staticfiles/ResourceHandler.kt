0d0a95cc51e56e1a2a42402ffa2ce76dc0ed71d8
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-04-19 01:13:55 +0800

    Add content length config to StaticFileConfig

--- javalin/src/main/java/io/javalin/http/staticfiles/ResourceHandler.kt
+++ javalin/src/main/java/io/javalin/http/staticfiles/ResourceHandler.kt
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 enum class Location { CLASSPATH, EXTERNAL; }
-data class StaticFileConfig(val path: String, val location: Location)
+data class StaticFileConfig(val path: String, val location: Location, var enforceContentLengthHeader: Boolean)
 
 interface ResourceHandler {
     fun handle(httpRequest: HttpServletRequest, httpResponse: HttpServletResponse): Boolean
