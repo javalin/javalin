0b57fbf54c2679b8059e05a2dd3ec8a4f67987db
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-05-11 15:56:21 +0800

    Add methods to PrecompressimgResourceHandler

--- javalin/src/main/java/io/javalin/http/staticfiles/PrecompressingResourceHandler.kt
+++ javalin/src/main/java/io/javalin/http/staticfiles/PrecompressingResourceHandler.kt
@@ -1,29 +1,60 @@
 package io.javalin.http.staticfiles
 
+import io.javalin.core.util.Header
+import io.javalin.http.Context
 import io.javalin.http.LeveledBrotliStream
 import io.javalin.http.LeveledGzipStream
+import org.eclipse.jetty.http.MimeTypes
 import org.eclipse.jetty.util.resource.Resource
 import java.io.*
 
-enum class CompressType(val type: String, val extension: String) {
-    GZIP("gzip", ".gz"),
-    BR("br", ".br")
-}
 
 class PrecompressingResourceHandler {
+
     val compressedFiles = HashMap<String, ByteArray>()
 
-    fun handle(resource: Resource, target: String): Boolean {
+    companion object {
+        val excludedMimeTypes = setOf(
+                "image/",
+                "audio/",
+                "video/",
+                "application/compress",
+                "application/zip",
+                "application/gzip",
+                "application/bzip2",
+                "application/brotli",
+                "application/x-xz",
+                "application/x-rar-compressed")
+    }
 
+    fun handle(resource: Resource, ctx: Context): Boolean {
+        if (resource.exists() && !resource.isDirectory) {
+            var acceptCompressType = CompressType.getByAcceptEncoding(ctx.header(Header.ACCEPT_ENCODING) ?: "")
+            val contentType = MimeTypes.getDefaultMimeByExtension(resource.file.name)//get content type by file extension
+            val resourceCompressible = !excludedMimeType(contentType)
+            if (!resourceCompressible)
+                acceptCompressType = CompressType.NONE
+            val resultByteArray = getStaticResourceByteArray(resource, ctx.path(), acceptCompressType, 4)
+
+            //TODO etag
+            //TODO compression level
+            //TODO request range
+
+            ctx.header(Header.CONTENT_TYPE, contentType)
+            ctx.header(Header.CONTENT_ENCODING, acceptCompressType.typeName)
+            ctx.result(resultByteArray)
+            
+            return true
+        }
         return false
     }
 
-    private fun getCompressedFileInputStream(originResource: Resource, target: String, type: CompressType, level: Int): InputStream {
+    private fun getStaticResourceByteArray(resource: Resource, target: String, type: CompressType, level: Int): ByteArray {
         val key = target + type.extension
         if (!compressedFiles.containsKey(key)) {
-            compressedFiles[key] = getCompressedByteArray(originResource, type, level)
+            compressedFiles[key] = getCompressedByteArray(resource, type, level)
         }
-        return compressedFiles[key]!!.inputStream()
+        return compressedFiles[key]!!
     }
 
     private fun getCompressedByteArray(resource: Resource, type: CompressType, level: Int): ByteArray {
@@ -36,14 +67,35 @@ class PrecompressingResourceHandler {
             CompressType.BR -> {
                 LeveledBrotliStream(byteArrayOutputStream, level)
             }
+            CompressType.NONE -> {
+                byteArrayOutputStream
+            }
         }
-        val buffer = ByteArray(2048)
-        var bytesRead: Int
-        while (fileInput.read(buffer).also { bytesRead = it } > 0) {
-            outputStream.write(buffer, 0, bytesRead)
-        }
+        fileInput.copyTo(outputStream, bufferSize = 2048)
         fileInput.close()
         outputStream.close()
         return byteArrayOutputStream.toByteArray()
+    }
+
+    private fun excludedMimeType(mimeType: String) =
+            if (mimeType == "") false else excludedMimeTypes.any { excluded -> mimeType.contains(excluded, ignoreCase = true) }
+
+    enum class CompressType(val typeName: String, val extension: String) {
+        GZIP("gzip", ".gz"),
+        BR("br", ".br"),
+        NONE("", "");
+
+        fun acceptEncoding(acceptEncoding: String): Boolean {
+            return acceptEncoding.contains(typeName, ignoreCase = true)
+        }
+
+        companion object {
+            fun getByAcceptEncoding(acceptEncoding: String): CompressType {
+                values().forEach {
+                    if (it.acceptEncoding(acceptEncoding)) return it
+                }
+                return NONE
+            }
+        }
     }
 }
