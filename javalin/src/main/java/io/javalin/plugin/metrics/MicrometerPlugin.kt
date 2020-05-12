740790e74f7bf02c9ebcb8ee8d18db8afab5211f
Jon Schneider <jschneider@gradle.com>
2020-05-12 08:22:20 -0400

    Improve Micrometer plugin to tag requests by URI, outcome, status, etc.

--- javalin/src/main/java/io/javalin/plugin/metrics/MicrometerPlugin.kt
+++ javalin/src/main/java/io/javalin/plugin/metrics/MicrometerPlugin.kt
@@ -2,24 +2,61 @@ package io.javalin.plugin.metrics
 
 import io.javalin.Javalin
 import io.javalin.core.plugin.Plugin
-import io.javalin.core.util.OptionalDependency
-import io.javalin.core.util.Util
+import io.javalin.http.Context
+import io.javalin.http.ExceptionHandler
+import io.javalin.http.HandlerEntry
+import io.javalin.http.HandlerType
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Metrics
 import io.micrometer.core.instrument.Tag
+import io.micrometer.core.instrument.Tags
+import io.micrometer.core.instrument.binder.http.DefaultHttpServletRequestTagsProvider
+import io.micrometer.core.instrument.binder.jetty.JettyConnectionMetrics
 import io.micrometer.core.instrument.binder.jetty.JettyServerThreadPoolMetrics
-import io.micrometer.core.instrument.binder.jetty.JettyStatisticsMetrics
-import org.eclipse.jetty.server.handler.StatisticsHandler
+import io.micrometer.core.instrument.binder.jetty.TimedHandler
+import org.apache.commons.lang3.StringUtils
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-class MicrometerPlugin @JvmOverloads constructor(val registry: MeterRegistry = Metrics.globalRegistry) : Plugin {
+class MicrometerPlugin @JvmOverloads constructor(private val registry: MeterRegistry = Metrics.globalRegistry, private val tags: Iterable<Tag> = Tags.empty()) : Plugin {
     override fun apply(app: Javalin) {
         app.server()?.server()?.let { server ->
-            Util.ensureDependencyPresent(OptionalDependency.MICROMETER)
-            val statisticsHandler = StatisticsHandler()
-            server.insertHandler(statisticsHandler)
-            JettyStatisticsMetrics.monitor(this.registry, statisticsHandler)
-            val threadPoolMetrics = JettyServerThreadPoolMetrics(server.threadPool, emptyList<Tag>())
-            threadPoolMetrics.bindTo(this.registry)
+            app.exception(Exception::class.java, EXCEPTION_HANDLER)
+
+            server.insertHandler(TimedHandler(registry, tags, object : DefaultHttpServletRequestTagsProvider() {
+                override fun getTags(request: HttpServletRequest, response: HttpServletResponse): Iterable<Tag> {
+                    val exceptionName = response.getHeader(EXCEPTION_HEADER)
+                    response.setHeader(EXCEPTION_HEADER, null)
+                    val uri = app.servlet().matcher
+                            .findEntries(HandlerType.GET, request.pathInfo)
+                            .stream()
+                            .findAny()
+                            .map(HandlerEntry::path)
+                            .map { path: String -> if (path == "/" || StringUtils.isBlank(path)) "root" else path }
+                            .map { path: String -> if (response.status in 300..399) "REDIRECTION" else path }
+                            .map { path: String -> if (response.status == 404) "NOT_FOUND" else path }
+                            .orElse("NOT_FOUND")
+                    return Tags.concat(
+                            super.getTags(request, response),
+                            "uri", uri,
+                            "exception", exceptionName ?: "None"
+                    )
+                }
+            }))
+
+            JettyServerThreadPoolMetrics(server.threadPool, tags).bindTo(registry)
+        }
+
+        JettyConnectionMetrics(registry, tags)
+    }
+
+    companion object {
+        private const val EXCEPTION_HEADER = "__micrometer_exception_name"
+
+        var EXCEPTION_HANDLER = ExceptionHandler { e: Exception, ctx: Context ->
+            val simpleName = e.javaClass.simpleName
+            ctx.header(EXCEPTION_HEADER, if (StringUtils.isNotBlank(simpleName)) simpleName else e.javaClass.name)
+            ctx.status(500)
         }
     }
 }
