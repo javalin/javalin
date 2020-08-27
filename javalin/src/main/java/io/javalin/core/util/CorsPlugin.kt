e5948b9481855f1901c17fc36570716557249aa4
Wilerson de Oliveira <5207+wilerson@users.noreply.github.com>
2020-08-27 14:31:21 +0200

    [cors-plugin] Allow the CORS plugin and OPTIONS endpoints to coexist (#1052)

--- javalin/src/main/java/io/javalin/core/util/CorsPlugin.kt
+++ javalin/src/main/java/io/javalin/core/util/CorsPlugin.kt
@@ -2,10 +2,7 @@ package io.javalin.core.util
 
 import io.javalin.Javalin
 import io.javalin.core.plugin.Plugin
-import io.javalin.core.security.CoreRoles
-import io.javalin.core.security.SecurityUtil.roles
 import io.javalin.http.util.CorsBeforeHandler
-import io.javalin.http.util.CorsOptionsHandler
 
 class CorsPlugin(private val origins: List<String>) : Plugin {
 
@@ -25,7 +22,6 @@ class CorsPlugin(private val origins: List<String>) : Plugin {
 
     override fun apply(app: Javalin) {
         app.before(CorsBeforeHandler(origins))
-        app.options("*", CorsOptionsHandler(), roles(CoreRoles.NO_WRAP))
     }
 
 }
