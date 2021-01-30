d65f05aada38a76b852de28506fa38483b42dd5b
David <davidaase@hotmail.com>
2021-01-30 09:44:32 +0100

    [ratelimiting] Add option to configure key function (and misc cleanup)

--- javalin/src/main/java/io/javalin/http/util/RateLimitUtil.kt
+++ javalin/src/main/java/io/javalin/http/util/RateLimitUtil.kt
@@ -10,47 +10,42 @@ import io.javalin.http.Context
 import io.javalin.http.HttpResponseException
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
-private val limiters = ConcurrentHashMap<TimeUnit, RateLimiter>()
+object RateLimitUtil {
+    val limiters = ConcurrentHashMap<TimeUnit, RateLimiter>()
+    var keyFunction: (Context) -> String = { ip(it) + it.method() + it.matchedPath() }
+    val executor: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
+    private fun ip(ctx: Context) = ctx.header("X-Forwarded-For")?.split(",")?.get(0) ?: ctx.ip()
+}
 
 class RateLimiter(val timeUnit: TimeUnit) {
 
-    private val handlerToIpToRequestCount = ConcurrentHashMap<String, Int>()
-
-    init {
-        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate({
-            handlerToIpToRequestCount.clear()
-        }, /*delay=*/0,  /*period=*/1, timeUnit)
+    private val timeUnitString = timeUnit.toString().toLowerCase().removeSuffix("s")
+    private val keyToRequestCount = ConcurrentHashMap<String, Int>().also {
+        RateLimitUtil.executor.scheduleAtFixedRate({ it.clear() }, /*delay=*/0,  /*period=*/1, timeUnit)
     }
 
     fun incrementCounter(ctx: Context, requestLimit: Int) {
-        val limiterName = ip(ctx) + ctx.method() + ctx.matchedPath()
-
-        handlerToIpToRequestCount.compute(limiterName) { _, count ->
+        keyToRequestCount.compute(RateLimitUtil.keyFunction(ctx)) { _, count ->
             when {
                 count == null -> 1
                 count < requestLimit -> count + 1
-                else -> throw HttpResponseException(
-                    429,
-                    "Rate limit exceeded - Server allows $requestLimit requests per ${
-                        timeUnit.toString().toLowerCase().removeSuffix("s")
-                    }."
-                )
+                else -> throw HttpResponseException(429, "Rate limit exceeded - Server allows $requestLimit requests per $timeUnitString.")
             }
         }
     }
 }
 
-private fun ip(ctx: Context) = ctx.header("X-Forwarded-For")?.split(",")?.get(0) ?: ctx.ip()
-
 class RateLimit(private val ctx: Context) {
     /**
-     * Simple IP-and-handler-based rate-limiting, activated by calling it in a [io.javalin.http.Handler].
+     * Simple key/count rate-limiting, activated by calling it in a [io.javalin.http.Handler].
      * All counters are cleared every [timeUnit].
+     * You can change the key by changing [RateLimitUtil.keyFunction], the default is ip + method + path
      * @throws HttpResponseException if the counter exceeds [numRequests] per [timeUnit]
      */
     fun requestPerTimeUnit(numRequests: Int, timeUnit: TimeUnit) {
-        limiters.computeIfAbsent(timeUnit) { RateLimiter(timeUnit) }.incrementCounter(ctx, numRequests)
+        RateLimitUtil.limiters.computeIfAbsent(timeUnit) { RateLimiter(timeUnit) }.incrementCounter(ctx, numRequests)
     }
 }
