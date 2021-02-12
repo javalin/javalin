9974025cd0402c0c0afd6cca4a3668793b9187f6
Joshua Swoyer <jswoyer@myriadmobile.com>
2021-02-12 14:56:36 -0600

    Removing now unnecessary annotations.

--- javalin/src/main/java/io/javalin/websocket/context/WsContext.kt
+++ javalin/src/main/java/io/javalin/websocket/context/WsContext.kt
@@ -3,11 +3,8 @@ package io.javalin.websocket.context
 import io.javalin.websocket.WsContext
 import io.javalin.websocket.WsMessageContext
 
-@JvmSynthetic
 inline fun <reified T : Any> WsContext.queryParam(key: String, default: String? = null) = queryParam(key, T::class.java, default)
 
-@JvmSynthetic
 inline fun <reified T : Any> WsContext.pathParam(key: String) = pathParam(key, T::class.java)
 
-@JvmSynthetic
 inline fun <reified T : Any> WsMessageContext.message(): T = message(T::class.java)
