cb4c3236d6d6554c3f35db25c15942b8f3ace57c
David <davidaase@hotmail.com>
2021-07-03 14:31:07 +0200

    javadoc for toJsonStream

--- javalin/src/main/java/io/javalin/plugin/json/JsonMapper.java
+++ javalin/src/main/java/io/javalin/plugin/json/JsonMapper.java
@@ -5,13 +5,26 @@ import kotlin.NotImplementedError;
 import org.jetbrains.annotations.NotNull;
 
 public interface JsonMapper {
-    default @NotNull <T> T fromJson(@NotNull String json, @NotNull Class<T> targetClass) {
+
+    @NotNull
+    default <T> T fromJson(@NotNull String json, @NotNull Class<T> targetClass) {
         throw new NotImplementedError("JsonMapper#fromJson not implemented");
     }
-    default @NotNull String toJson(@NotNull Object obj) {
+
+    @NotNull
+    default String toJson(@NotNull Object obj) {
         throw new NotImplementedError("JsonMapper#toJson not implemented");
     }
-    default @NotNull InputStream toJsonStream(@NotNull Object obj) {
-        throw new NotImplementedError("JsonMapper#toJson not implemented");
+
+    /**
+     * Javalin needs an InputStream in order to finish up the response
+     * properly, which can be a little challenging if your JSON library
+     * wants to write to an OutputStream. You can look at JavalinJackson,
+     * which uses a PipedStreamUtil to get an InputStream from an OutputStream.
+     */
+    @NotNull
+    default InputStream toJsonStream(@NotNull Object obj) {
+        throw new NotImplementedError("JsonMapper#toJsonStream not implemented");
     }
+
 }
