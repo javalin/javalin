c43c685a14f58a55aaea24e2a3f583b1f8bfc9c6
David <davidaase@hotmail.com>
2021-07-04 16:47:46 +0200

    add javadoc

--- javalin/src/main/java/io/javalin/plugin/json/JsonMapper.java
+++ javalin/src/main/java/io/javalin/plugin/json/JsonMapper.java
@@ -6,13 +6,22 @@ import org.jetbrains.annotations.NotNull;
 
 public interface JsonMapper {
 
+    /**
+     * If {@link #toJsonStream(Object)} is not implemented, Javalin will use this method
+     * when sending JSON data to a client through {@link io.javalin.http.Context#json(Object)}.
+     * Regardless of if {@link #toJsonStream(Object)} is implemented, Javalin will use this
+     * method in the CookieStore class, for WebSockets messaging, and in JavalinVue.
+     */
     @NotNull
     default String toJsonString(@NotNull Object obj) {
         throw new NotImplementedError("JsonMapper#toJsonString not implemented");
     }
 
     /**
-     * If implemented, Javalin will choose this method over toJsonString.
+     * You can implement this method to reduce memory usage.
+     *
+     * If implemented, Javalin will use this method instead of {@link #toJsonString(Object)}
+     * when sending JSON data to a client through {@link io.javalin.http.Context#json(Object)}.
      * Javalin requires an InputStream in order to finish up the response properly.
      * Use (or look at) PipedStreamUtil to get an InputStream from an OutputStream.
      */
@@ -21,13 +30,22 @@ public interface JsonMapper {
         throw new NotImplementedError("JsonMapper#toJsonStream not implemented");
     }
 
+    /**
+     * If {@link #fromJsonStream(InputStream, Class)} is not implemented, Javalin will use this method
+     * when mapping request bodies to JSON through {@link io.javalin.http.Context#bodyAsClass(Class)}.
+     * Regardless of if {@link #fromJsonStream(InputStream, Class)} is implemented, Javalin will use this
+     * method in the {@link io.javalin.core.validation.BaseValidator)} class.
+     */
     @NotNull
     default <T> T fromJsonString(@NotNull String json, @NotNull Class<T> targetClass) {
         throw new NotImplementedError("JsonMapper#fromJsonString not implemented");
     }
 
     /**
-     * If implemented, Javalin will choose this method over fromJsonString.
+     * You can implement this method to reduce memory usage.
+     *
+     * If implemented, Javalin will use this method instead of {@link #fromJsonString(String, Class)}
+     * when mapping request bodies to JSON through {@link io.javalin.http.Context#bodyAsClass(Class)}.
      */
     @NotNull
     default <T> T fromJsonStream(@NotNull InputStream json, @NotNull Class<T> targetClass) {
