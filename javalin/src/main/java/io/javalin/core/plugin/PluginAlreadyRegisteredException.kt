ed176e9f141b416f261c376e7a33d9f477902695
David <davidaase@hotmail.com>
2021-07-06 01:10:00 +0200

    [housekeeping] Cleanup compression and plugin packages

--- javalin/src/main/java/io/javalin/core/plugin/PluginAlreadyRegisteredException.kt
+++
@@ -1,4 +0,0 @@
-package io.javalin.core.plugin
-
-data class PluginAlreadyRegisteredException(val pluginClass: Class<out Plugin>)
-    : RuntimeException("${pluginClass.canonicalName} is already registered")
