d3ade3ba9a97446cca9ff739eaa068e9ee2c2fd2
David <davidaase@hotmail.com>
2021-07-16 17:37:49 +0200

    [staticfiles] Simplify map declaration

--- javalin/src/main/java/io/javalin/http/staticfiles/StaticFileConfig.kt
+++ javalin/src/main/java/io/javalin/http/staticfiles/StaticFileConfig.kt
@@ -2,7 +2,6 @@ package io.javalin.http.staticfiles
 
 import io.javalin.core.util.Header
 import org.eclipse.jetty.server.handler.ContextHandler.AliasCheck
-import java.util.*
 
 data class StaticFileConfig(
     @JvmField var hostedPath: String = "/",
@@ -10,5 +9,5 @@ data class StaticFileConfig(
     @JvmField var location: Location = Location.CLASSPATH,
     @JvmField var precompress: Boolean = false,
     @JvmField var aliasCheck: AliasCheck? = null,
-    @JvmField var headers: Map<String, String> = HashMap(Collections.singletonMap(Header.CACHE_CONTROL, "max-age=0"))
+    @JvmField var headers: Map<String, String> = mutableMapOf(Header.CACHE_CONTROL to "max-age=0")
 )
