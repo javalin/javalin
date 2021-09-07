c64223747ee4381f78e791e20fe90be96d78ffc7
dzikoysk <dzikoysk@dzikoysk.net>
2021-09-07 16:37:38 +0200

    GH-1377 Remove goto-like exception based credentials matching in BasicAuthFilter

--- javalin/src/main/java/io/javalin/core/security/BasicAuthFilter.kt
+++ javalin/src/main/java/io/javalin/core/security/BasicAuthFilter.kt
@@ -9,22 +9,21 @@ package io.javalin.core.security
 import io.javalin.Javalin
 import io.javalin.core.plugin.Plugin
 import io.javalin.core.util.Header
-import io.javalin.core.util.JavalinException
 import io.javalin.http.UnauthorizedResponse
 
 // adds a filter that runs before every http request (does not apply to websocket upgrade requests)
 class BasicAuthFilter(private val username: String, private val password: String) : Plugin {
+
     override fun apply(app: Javalin) {
         app.before { ctx ->
-            try {
-                val (user, pass) = ctx.basicAuthCredentials()
-                if (user != username || pass != password) {
-                    throw JavalinException("Incorrect username or password")
-                }
-            } catch (e: Exception) { // badly formatted header OR incorrect credentials
+            val matched = runCatching { ctx.basicAuthCredentials() }
+                .fold({ (user, pass) -> (user == username && pass == password) }, { false })
+
+            if (matched.not()) {
                 ctx.header(Header.WWW_AUTHENTICATE, "Basic")
                 throw UnauthorizedResponse()
             }
         }
     }
+
 }
