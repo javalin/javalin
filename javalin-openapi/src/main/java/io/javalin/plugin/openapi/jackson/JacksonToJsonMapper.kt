9d0c3ce9cfea756f78499ee086156f0c076708b2
Loris Sauter <loris.sauter@unibas.ch>
2021-10-13 20:01:37 +0200

    Added Jackson Mixins for Schema and MediaType in default JacksonToJsonMapper

--- javalin-openapi/src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
+++ javalin-openapi/src/main/java/io/javalin/plugin/openapi/jackson/JacksonToJsonMapper.kt
@@ -3,7 +3,11 @@ package io.javalin.plugin.openapi.jackson
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
 import io.javalin.plugin.openapi.utils.LazyDefaultValue
+import io.swagger.v3.core.jackson.mixin.MediaTypeMixin
+import io.swagger.v3.core.jackson.mixin.SchemaMixin
 import io.swagger.v3.core.util.Json
+import io.swagger.v3.oas.models.media.MediaType
+import io.swagger.v3.oas.models.media.Schema
 
 /**
  * Default jackson mapper for creating the object api schema json.
@@ -19,6 +23,8 @@ class JacksonToJsonMapper(
     companion object {
         val defaultObjectMapper: ObjectMapper by LazyDefaultValue {
             Json.mapper().registerModule(kotlinModule())
+                    .addMixIn(Schema::class.java, SchemaMixin::class.java)
+                    .addMixIn(MediaType::class.java, MediaTypeMixin::class.java)
         }
     }
 
