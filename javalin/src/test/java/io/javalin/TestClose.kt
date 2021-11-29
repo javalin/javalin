984309310200bfb755737b72a8537df0610f776c
Signed Bit <signedbit@users.noreply.github.com>
2021-11-29 08:58:39 +0000

    [javalin] Enhance Javalin class to support try with resources (#1441)

---
+++ javalin/src/test/java/io/javalin/TestClose.kt
@@ -0,0 +1,49 @@
+/*
+ * Javalin - https://javalin.io
+ * Copyright 2017 David Åse
+ * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ *
+ */
+package io.javalin
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TestClose {
+
+    @Test
+    fun useStopsServer() {
+        val app = Javalin.create()
+
+        app.start(0).use { }
+
+        assertThat(app.jettyServer.server().isStopped).isTrue
+    }
+
+    @Test
+    fun useCallsLifecycleEvents() {
+        var log = ""
+        val app = Javalin.create().events {
+            it.serverStopping { log += "Stopping" }
+            it.serverStopped { log += "Stopped" }
+        }
+
+        app.start(0).use { }
+
+        assertThat(log).isEqualTo("StoppingStopped")
+    }
+
+    @Test
+    fun closingInsideUseIsIdempotent() {
+        var log = ""
+        val app = Javalin.create().events {
+            it.serverStopping { log += "Stopping" }
+            it.serverStopped { log += "Stopped" }
+        }
+
+        app.start(0).use { it.close() }
+
+        assertThat(app.jettyServer.server().isStopped).isTrue
+        assertThat(log).isEqualTo("StoppingStopped")
+    }
+}
