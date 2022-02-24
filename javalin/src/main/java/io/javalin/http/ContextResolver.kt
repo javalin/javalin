7b6541b38540852657ed67a6125a96c7c7d7253c
David <davidaase@hotmail.com>
2022-02-24 14:14:29 +0100

    [config] Make context resolvers jvm fields

--- javalin/src/main/java/io/javalin/http/ContextResolver.kt
+++ javalin/src/main/java/io/javalin/http/ContextResolver.kt
@@ -9,9 +9,11 @@ const val CONTEXT_RESOLVER_KEY = "contextResolver"
 fun Context.contextResolver() = this.appAttribute<ContextResolver>(CONTEXT_RESOLVER_KEY)
 
 class ContextResolver {
-    var ip: (Context) -> String = { it.req.remoteAddr }
-    var host: (Context) -> String? = { it.header(Header.HOST) }
-    var scheme: (Context) -> String = { it.req.scheme }
-    var url: (Context) -> String = { it.req.requestURL.toString() }
-    var fullUrl: (Context) -> String = { it.url() + if (it.queryString() != null) "?" + it.queryString() else "" }
+    // @formatter:off
+    @JvmField var ip: (Context) -> String = { it.req.remoteAddr }
+    @JvmField var host: (Context) -> String? = { it.header(Header.HOST) }
+    @JvmField var scheme: (Context) -> String = { it.req.scheme }
+    @JvmField var url: (Context) -> String = { it.req.requestURL.toString() }
+    @JvmField var fullUrl: (Context) -> String = { it.url() + if (it.queryString() != null) "?" + it.queryString() else "" }
+    // @formatter:on
 }
