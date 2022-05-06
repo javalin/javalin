371579f6bc8d14b1a91c1078f22a3767536f001b
tipsy <davidaase@hotmail.com>
2022-05-06 23:36:08 +0200

    [cleanup] Use the internal HttpCode class rather than Jetty's

--- javalin/src/main/java/io/javalin/http/HttpResponseException.kt
+++ javalin/src/main/java/io/javalin/http/HttpResponseException.kt
@@ -6,12 +6,10 @@
 
 package io.javalin.http
 
-import org.eclipse.jetty.http.HttpStatus
-
 open class HttpResponseException @JvmOverloads constructor(val status: Int, message: String, val details: Map<String, String> = mapOf()) : RuntimeException(message)
 
 class RedirectResponse @JvmOverloads constructor(
-    status: Int = HttpStatus.FOUND_302,
+    status: Int = HttpCode.FOUND.status,
     message: String = "Redirected",
     details: Map<String, String> = mapOf()
 ) : HttpResponseException(status, message, details)
@@ -20,54 +18,54 @@ class RedirectResponse @JvmOverloads constructor(
 class BadRequestResponse @JvmOverloads constructor(
     message: String = "Bad request",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.BAD_REQUEST_400, message, details)
+) : HttpResponseException(HttpCode.BAD_REQUEST.status, message, details)
 
 class UnauthorizedResponse @JvmOverloads constructor(
     message: String = "Unauthorized",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.UNAUTHORIZED_401, message, details)
+) : HttpResponseException(HttpCode.UNAUTHORIZED.status, message, details)
 
 class ForbiddenResponse @JvmOverloads constructor(
     message: String = "Forbidden",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.FORBIDDEN_403, message, details)
+) : HttpResponseException(HttpCode.FORBIDDEN.status, message, details)
 
 class NotFoundResponse @JvmOverloads constructor(
     message: String = "Not found",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.NOT_FOUND_404, message, details)
+) : HttpResponseException(HttpCode.NOT_FOUND.status, message, details)
 
 class MethodNotAllowedResponse @JvmOverloads constructor(
     message: String = "Method not allowed",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.METHOD_NOT_ALLOWED_405, message, details)
+) : HttpResponseException(HttpCode.METHOD_NOT_ALLOWED.status, message, details)
 
 class ConflictResponse @JvmOverloads constructor(
     message: String = "Conflict",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.CONFLICT_409, message, details)
+) : HttpResponseException(HttpCode.CONFLICT.status, message, details)
 
 class GoneResponse @JvmOverloads constructor(
     message: String = "Gone",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.GONE_410, message, details)
+) : HttpResponseException(HttpCode.GONE.status, message, details)
 
 class InternalServerErrorResponse @JvmOverloads constructor(
     message: String = "Internal server error",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.INTERNAL_SERVER_ERROR_500, message, details)
+) : HttpResponseException(HttpCode.INTERNAL_SERVER_ERROR.status, message, details)
 
 class BadGatewayResponse @JvmOverloads constructor(
     message: String = "Bad gateway",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.BAD_GATEWAY_502, message, details)
+) : HttpResponseException(HttpCode.BAD_GATEWAY.status, message, details)
 
 class ServiceUnavailableResponse @JvmOverloads constructor(
     message: String = "Service unavailable",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.SERVICE_UNAVAILABLE_503, message, details)
+) : HttpResponseException(HttpCode.SERVICE_UNAVAILABLE.status, message, details)
 
 class GatewayTimeoutResponse @JvmOverloads constructor(
     message: String = "Gateway timeout",
     details: Map<String, String> = mapOf()
-) : HttpResponseException(HttpStatus.GATEWAY_TIMEOUT_504, message, details)
+) : HttpResponseException(HttpCode.GATEWAY_TIMEOUT.status, message, details)
