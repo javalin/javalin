5ff7ecff4b6723e72dc47c5bf718025404464bdb
Luiz Alcides Zytkowski Junior <luiz.alcides@softniels.com.br>
2022-05-23 14:35:27 -0300

    JavaApp and KotlinApp: Registered `/listen` method.

--- javalin-testtools/src/test/java/io/javalin/testtools/JavaApp.java
+++ javalin-testtools/src/test/java/io/javalin/testtools/JavaApp.java
@@ -2,7 +2,10 @@ package io.javalin.testtools;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import io.javalin.http.sse.SseClient;
+
 import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.sse;
 
 // We're using statics for simplicity's sake, but you could
 // make it non static and do dependency injection or whatever
@@ -12,11 +15,24 @@ public class JavaApp {
         javalin.ignoreTrailingSlashes = false;
     }).routes(() -> {
         get("/hello", HelloController::hello);
+        sse("/listen", HelloController::sayHelloFiveTimes);
     });
 
     static class HelloController {
         public static void hello(Context ctx) {
             ctx.result("Hello, app!");
+        }
+
+        public static void sayHelloFiveTimes(SseClient sseClient) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    sseClient.sendEvent("Hello!");
+                    Thread.sleep(200);
+                }
+                sseClient.close();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
--- javalin-testtools/src/test/kotlin/io/javalin/testtools/KotlinApp.kt
+++ javalin-testtools/src/test/kotlin/io/javalin/testtools/KotlinApp.kt
@@ -3,6 +3,7 @@ package io.javalin.testtools
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder
 import io.javalin.http.Context
+import io.javalin.http.sse.SseClient
 
 // We're using objects for simplicity's sake, but you could
 // make it classes and do dependency injection or whatever
@@ -11,11 +12,20 @@ object KotlinApp {
         javalin.ignoreTrailingSlashes = false
     }.routes {
         ApiBuilder.get("/hello", HelloController::hello)
+        ApiBuilder.sse("/listen", HelloController::sayHelloFiveTimes)
     }
 
     internal object HelloController {
         fun hello(ctx: Context) {
             ctx.result("Hello, app!")
+        }
+
+        fun sayHelloFiveTimes(sseClient: SseClient) {
+            repeat(5) {
+                sseClient.sendEvent("Hello!")
+                Thread.sleep(200)
+            }
+            sseClient.close()
         }
     }
 }
