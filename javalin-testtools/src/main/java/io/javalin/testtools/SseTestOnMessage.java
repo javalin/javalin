38c31e8b90ad589dff3875a956f96fd585121c58
Junior <junior.zytkowski@gmail.com>
2022-05-23 20:28:03 -0300

    SseTestOnMessage: `process` now consumes `SseEvent` instead

--- javalin-testtools/src/main/java/io/javalin/testtools/SseTestOnMessage.java
+++ javalin-testtools/src/main/java/io/javalin/testtools/SseTestOnMessage.java
@@ -1,9 +1,7 @@
 package io.javalin.testtools;
 
-import okhttp3.sse.EventSource;
-
 public interface SseTestOnMessage {
 
-    void process(EventSource eventSource, String id, String type, String data);
+    void process(SseEvent sseEvent);
 
 }
