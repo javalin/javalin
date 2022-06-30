4b94b84e42143a923f8b6eb1f4d89a789d321787
David <davidaase@hotmail.com>
2022-07-01 01:19:58 +0200

    [cleanup] Refactor executor setup for ws pings

--- javalin/src/main/java/io/javalin/core/util/JavalinExecutors.kt
+++ javalin/src/main/java/io/javalin/core/util/JavalinExecutors.kt
@@ -1,18 +1,24 @@
 package io.javalin.core.util
 
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.ThreadFactory
 import java.util.concurrent.atomic.AtomicInteger
 
 object JavalinExecutors {
     @JvmStatic
-    fun newCachedThreadPool(name: String) = Executors.newCachedThreadPool(NamedThreadFactory(name))
+    fun newCachedThreadPool(name: String): ExecutorService =
+        Executors.newCachedThreadPool(NamedThreadFactory(name))
+
+    fun newSingleThreadScheduledExecutor(name: String): ScheduledExecutorService =
+        Executors.newSingleThreadScheduledExecutor(NamedThreadFactory(name))
 }
 
 class NamedThreadFactory(private val prefix: String) : ThreadFactory {
     private val group = Thread.currentThread().threadGroup
     private val threadCount = AtomicInteger(0)
-    override fun newThread(runnalbe: Runnable): Thread =
-        Thread(group, runnalbe, "$prefix - ${threadCount.getAndIncrement()}", 0)
+    override fun newThread(runnable: Runnable): Thread =
+        Thread(group, runnable, "$prefix-${threadCount.getAndIncrement()}", 0)
 
 }
--- javalin/src/main/java/io/javalin/websocket/WsAutomaticPing.kt
+++ javalin/src/main/java/io/javalin/websocket/WsAutomaticPing.kt
@@ -1,21 +1,21 @@
 package io.javalin.websocket
 
+import io.javalin.core.util.JavalinExecutors
 import java.nio.ByteBuffer
-import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
-
-var executor: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
+val executor: ScheduledExecutorService = JavalinExecutors.newSingleThreadScheduledExecutor("JavalinWebSocketPingThread")
 
 fun enableAutomaticPings(ctx: WsContext, interval: Long, unit: TimeUnit, applicationData: ByteBuffer?) {
     synchronized(ctx) {
         disableAutomaticPings(ctx);
         ctx.pingFuture = executor.scheduleAtFixedRate({
-            ctx.sendPing(applicationData);
-        }, interval, interval, unit);
+            ctx.sendPing(applicationData)
+        }, interval, interval, unit)
     }
 }
+
 fun disableAutomaticPings(ctx: WsContext) {
     synchronized(ctx) {
         ctx.pingFuture?.cancel(false)
