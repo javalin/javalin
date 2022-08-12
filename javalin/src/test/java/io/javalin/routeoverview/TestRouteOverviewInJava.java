29f97f28248acfba9d97201cd1598636b3aa7174
David <davidaase@hotmail.com>
2022-08-12 16:00:08 +0200

    [deps] Remove last bit of hamcrest

--- javalin/src/test/java/io/javalin/routeoverview/TestRouteOverviewInJava.java
+++ javalin/src/test/java/io/javalin/routeoverview/TestRouteOverviewInJava.java
@@ -12,8 +12,7 @@ import io.javalin.plugin.bundled.RouteOverviewUtil;
 import io.javalin.routeoverview.VisualTest.HandlerImplementation;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TestRouteOverviewInJava {
 
@@ -21,18 +20,18 @@ public class TestRouteOverviewInJava {
 
     @Test
     public void field_works() {
-        assertThat(RouteOverviewUtil.getMetaInfo(lambdaField), is("io.javalin.routeoverview.TestRouteOverviewInJava.lambdaField"));
+        assertThat(RouteOverviewUtil.getMetaInfo(lambdaField)).isEqualTo("io.javalin.routeoverview.TestRouteOverviewInJava.lambdaField");
     }
 
     @Test
     public void class_works() {
-        assertThat(RouteOverviewUtil.getMetaInfo(new InnerHandlerImplementation()), is("io.javalin.routeoverview.TestRouteOverviewInJava$InnerHandlerImplementation.class"));
-        assertThat(RouteOverviewUtil.getMetaInfo(new HandlerImplementation()), is("io.javalin.routeoverview.VisualTest$HandlerImplementation.class"));
+        assertThat(RouteOverviewUtil.getMetaInfo(new InnerHandlerImplementation())).isEqualTo("io.javalin.routeoverview.TestRouteOverviewInJava$InnerHandlerImplementation.class");
+        assertThat(RouteOverviewUtil.getMetaInfo(new HandlerImplementation())).isEqualTo("io.javalin.routeoverview.VisualTest$HandlerImplementation.class");
     }
 
     @Test
     public void lambda_works() {
-        assertThat(RouteOverviewUtil.getMetaInfo((Handler) (ctx -> ctx.result(""))), is("io.javalin.routeoverview.TestRouteOverviewInJava::??? (anonymous lambda)"));
+        assertThat(RouteOverviewUtil.getMetaInfo((Handler) (ctx -> ctx.result("")))).isEqualTo("io.javalin.routeoverview.TestRouteOverviewInJava::??? (anonymous lambda)");
     }
 
     private static class InnerHandlerImplementation implements Handler {
