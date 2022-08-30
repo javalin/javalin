2edc0e715c04145fd3fed26687f667157a94b582
dzikoysk <dzikoysk@dzikoysk.net>
2022-08-30 13:49:50 +0200

    Move `AsyncContext` handling to standalone function and add a little of syntax sugar

--- javalin/src/main/java/io/javalin/http/servlet/JavalinServlet.kt
+++ javalin/src/main/java/io/javalin/http/servlet/JavalinServlet.kt
@@ -15,6 +15,7 @@ import io.javalin.routing.PathMatcher
 import jakarta.servlet.http.HttpServlet
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import java.util.concurrent.CompletableFuture
 
 class JavalinServlet(val cfg: JavalinConfig) : HttpServlet() {
 
@@ -30,76 +31,80 @@ class JavalinServlet(val cfg: JavalinConfig) : HttpServlet() {
             val requestUri = ctx.path().removePrefix(ctx.contextPath())
             cfg.pvt.servletRequestLifecycle.forEach { it.createTasks(submitTask, this, ctx, requestUri) }
 
-            handleSync(ctx)
+            ctx.handleSync()
         } catch (throwable: Throwable) {
             exceptionMapper.handleUnexpectedThrowable(response, throwable)
         }
     }
 
-    private fun handleSync(ctx: JavalinServletContext) {
-        while (ctx.userFutureSupplier == null && ctx.tasks.isNotEmpty()) {
-            val task = ctx.tasks.poll()
-            if (ctx.exceptionOccurred && task.skipIfErrorOccurred) {
+    private fun JavalinServletContext.handleSync() {
+        while (userFutureSupplier == null && tasks.isNotEmpty()) {
+            val task = tasks.poll()
+            if (exceptionOccurred && task.skipIfErrorOccurred) {
                 continue
             }
-            handleTask(ctx, task.handler)
+            handleTask(task.handler)
         }
-
         when {
-            ctx.userFutureSupplier != null -> handleAsync(ctx)
-            else -> writeResponseAndLog(ctx)
+            userFutureSupplier != null -> handleAsync()
+            else -> writeResponseAndLog()
         }
     }
 
-    private fun handleAsync(ctx: JavalinServletContext) {
-        val userFutureSupplier = ctx.userFutureSupplier ?: return
-        ctx.userFutureSupplier = null
-        val userFuture = handleTask(ctx) { userFutureSupplier.value } ?: return handleSync(ctx) // get future from supplier or handle error
+    private fun JavalinServletContext.handleAsync() {
+        val userFutureSupplier = userFutureSupplier ?: return
+        this.userFutureSupplier = null
+        val userFuture = handleTask { userFutureSupplier.value } ?: return handleSync() // get future from supplier or handle error
 
-        if (!ctx.isAsync()) {
-            ctx.req().startAsync().addListener(onTimeout = { // a timeout avoids the pipeline - we need to handle it manually + it's not thread-safe
-                ctx.status(INTERNAL_SERVER_ERROR) // default error handling
-                errorMapper.handle(ctx.statusCode(), ctx) // user defined error handling
-                if (ctx.resultStream() == null) ctx.result("Request timed out") // write default response only if handler didn't do anything
-                writeResponseAndLog(ctx)
-            }).also { asyncCtx -> asyncCtx.timeout = cfg.http.asyncTimeout }
-        }
-        if (ctx.isAsync()) {
-            ctx.req().asyncContext.addListener(onTimeout = { userFuture.cancel(true) }) // registers timeout listener
-        }
-
-        userFuture
-            .thenApply { handleSync(ctx) }
+        handleAsyncContextAndAddDefaultTimeoutListeners(userFuture)
+            .thenApply { handleSync() }
             .exceptionally {
-                exceptionMapper.handle(ctx, it)
-                writeResponseAndLog(ctx)
+                exceptionMapper.handle(this, it)
+                writeResponseAndLog()
             }
     }
 
-    private fun <R> handleTask(ctx: JavalinServletContext, handler: TaskHandler<R>): R? =
+    private fun JavalinServletContext.handleAsyncContextAndAddDefaultTimeoutListeners(userFuture: CompletableFuture<*>) = userFuture.also {
+        if (!isAsync()) {
+            req().startAsync().also {
+                it.addListener(onTimeout = { // a timeout avoids the pipeline - we need to handle it manually + it's not thread-safe
+                    status(INTERNAL_SERVER_ERROR) // default error handling
+                    errorMapper.handle(statusCode(), this) // user defined error handling
+                    if (resultStream() == null) result("Request timed out") // write default response only if handler didn't do anything
+                    writeResponseAndLog()
+                })
+                it.timeout = cfg.http.asyncTimeout
+            }
+        }
+        if (isAsync()) {
+            req().asyncContext.addListener(onTimeout = { userFuture.cancel(true) }) // registers timeout listener
+        }
+    }
+
+    private fun <R> JavalinServletContext.handleTask(handler: TaskHandler<R>): R? =
         try {
             handler.handle()
         } catch (throwable: Throwable) {
-            ctx.exceptionOccurred = true
-            ctx.userFutureSupplier = null
-            ctx.tasks.offerFirst(Task(skipIfErrorOccurred = false) { exceptionMapper.handle(ctx, throwable) })
+            this.exceptionOccurred = true
+            this.userFutureSupplier = null
+            this.tasks.offerFirst(Task(skipIfErrorOccurred = false) { exceptionMapper.handle(this, throwable) })
             null
         }
 
-    private fun writeResponseAndLog(ctx: JavalinServletContext) {
+    private fun JavalinServletContext.writeResponseAndLog() {
         try {
-            if (ctx.responseWritten.getAndSet(true)) return // prevent writing more than once, it's required because timeout listener can terminate the flow at any time
-            ctx.outputStream().use { outputStream ->
-                ctx.resultStream()?.use { resultStream ->
-                    val etagWritten = ETagGenerator.tryWriteEtagAndClose(cfg.http.generateEtags, ctx, resultStream)
+            if (responseWritten.getAndSet(true)) return // prevent writing more than once, it's required because timeout listener can terminate the flow at any time
+            outputStream().use { outputStream ->
+                resultStream()?.use { resultStream ->
+                    val etagWritten = ETagGenerator.tryWriteEtagAndClose(cfg.http.generateEtags, this, resultStream)
                     if (!etagWritten) resultStream.copyTo(outputStream, 4096)
                 }
             }
-            cfg.pvt.requestLogger?.handle(ctx, ctx.executionTimeMs())
+            cfg.pvt.requestLogger?.handle(this, executionTimeMs())
         } catch (throwable: Throwable) {
-            exceptionMapper.handleUnexpectedThrowable(ctx.res(), throwable) // handle any unexpected error, e.g. write failure
+            exceptionMapper.handleUnexpectedThrowable(res(), throwable) // handle any unexpected error, e.g. write failure
         } finally {
-            if (ctx.isAsync()) ctx.req().asyncContext.complete() // guarantee completion of async context to eliminate the possibility of hanging connections
+            if (isAsync()) req().asyncContext.complete() // guarantee completion of async context to eliminate the possibility of hanging connections
         }
     }
 
