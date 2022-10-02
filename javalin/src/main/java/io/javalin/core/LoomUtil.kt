22190f09f2c3a0eb287d9cf21413c5502f3b1a8a
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2022-10-02 18:39:27 +0200

    [loom] Try to get executor instead of just checking for existence

--- javalin/src/main/java/io/javalin/core/LoomUtil.kt
+++ javalin/src/main/java/io/javalin/core/LoomUtil.kt
@@ -5,31 +5,40 @@ import java.util.concurrent.Executors
 import java.util.concurrent.ThreadFactory
 import java.util.concurrent.atomic.AtomicInteger
 
-internal class NamedThreadFactory(private val prefix: String) : ThreadFactory {
-    private val group = Thread.currentThread().threadGroup
-    private val threadCount = AtomicInteger(0)
-    override fun newThread(runnable: Runnable): Thread =
-        Thread(group, runnable, "$prefix-${threadCount.getAndIncrement()}", 0)
-
-}
-
 object LoomUtil {
 
     @JvmField
     var useLoomThreadPool = true
 
-    val loomAvailable = try {
-        Thread::class.java.getMethod("startVirtualThread", Runnable::class.java)
-        Executors::class.java.getMethod("newThreadPerTaskExecutor", ThreadFactory::class.java)
-        true
-    } catch (e: Exception) {
-        false
-    }
+    val loomAvailable = runCatching {
+        val factoryMethod = Executors::class.java.getMethod("newVirtualThreadPerTaskExecutor") // this will not throw if preview is not enabled
+        factoryMethod.invoke(Executors::class.java) as ExecutorService // this *will* throw if preview is not enabled
+    }.isSuccess
 
     fun getExecutorService(name: String): ExecutorService {
         require(loomAvailable) { "Your Java version (${System.getProperty("java.version")}) doesn't support Loom" }
         val factoryMethod = Executors::class.java.getMethod("newThreadPerTaskExecutor", ThreadFactory::class.java)
-        return factoryMethod.invoke(Executors::class.java, NamedThreadFactory(name)) as ExecutorService
+        return factoryMethod.invoke(Executors::class.java, NamedVirtualThreadFactory(name)) as ExecutorService
     }
 
+}
+
+private class NamedVirtualThreadFactory(private val prefix: String) : ThreadFactory {
+
+    private val threadCount = AtomicInteger(0)
+    override fun newThread(runnable: Runnable): Thread = ReflectiveVirtualThreadBuilder()
+        .name("$prefix-Virtual-${threadCount.getAndIncrement()}")
+        .unstarted(runnable)
+
+    private class ReflectiveVirtualThreadBuilder {
+        private val builderClass = Class.forName("java.lang.Thread\$Builder\$OfVirtual")
+        private var virtualBuilder = Thread::class.java.getMethod("ofVirtual").invoke(Thread::class.java)
+
+        fun name(name: String): ReflectiveVirtualThreadBuilder = also {
+            this.virtualBuilder = builderClass.getMethod("name", String::class.java).invoke(virtualBuilder, name)
+        }
+
+        fun unstarted(runnable: Runnable): Thread =
+            builderClass.getMethod("unstarted", Runnable::class.java).invoke(virtualBuilder, runnable) as Thread
+    }
 }
