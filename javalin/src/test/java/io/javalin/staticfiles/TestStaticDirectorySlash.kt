f6f8e68739a26d4aee36fcd6e4310d60e879d352
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2022-10-17 16:44:44 +0200

    [static-files] Fix welcome files when `precompress` enabled (#1713)

--- javalin/src/test/java/io/javalin/staticfiles/TestStaticDirectorySlash.kt
+++ javalin/src/test/java/io/javalin/staticfiles/TestStaticDirectorySlash.kt
@@ -19,6 +19,15 @@ class TestStaticDirectorySlash {
 
     private val normalJavalin: Javalin by lazy { Javalin.create { it.staticFiles.add("public", Location.CLASSPATH) } }
 
+    private val precompressingJavalin: Javalin by lazy {
+        Javalin.create {
+            it.staticFiles.add{
+                it.directory = "public"
+                it.location = Location.CLASSPATH
+                it.precompress = true
+            }
+        }
+    }
     @Test
     fun `normal javalin ignores static directory slashes`() = TestUtil.test(normalJavalin) { _, http ->
         assertThat(http.getBody("/subpage")).isEqualTo("TEST") // ok, is directory
@@ -26,7 +35,20 @@ class TestStaticDirectorySlash {
     }
 
     @Test
+    fun `precompressing javalin ignores static directory slashes`() = TestUtil.test(precompressingJavalin) { _, http ->
+        assertThat(http.getBody("/subpage")).isEqualTo("TEST") // ok, is directory
+        assertThat(http.getBody("/subpage/")).isEqualTo("TEST") // ok, is directory
+    }
+
+
+    @Test
     fun `normal Javalin serves files but serves directory if it is a directory`() = TestUtil.test(normalJavalin) { _, http ->
+        assertThat(http.getBody("/file")).isEqualTo("TESTFILE") // ok, is file = no slash
+        assertThat(http.getBody("/file/")).isEqualTo(NOT_FOUND.message) // nope, has slash must be directory
+    }
+
+    @Test
+    fun `precompressing Javalin serves files but serves directory if it is a directory`() = TestUtil.test(precompressingJavalin) { _, http ->
         assertThat(http.getBody("/file")).isEqualTo("TESTFILE") // ok, is file = no slash
         assertThat(http.getBody("/file/")).isEqualTo(NOT_FOUND.message) // nope, has slash must be directory
     }
