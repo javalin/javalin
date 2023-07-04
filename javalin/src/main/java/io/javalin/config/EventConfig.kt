422296af72e235d4ae2f0796d43cbdc117ef14a7
dzikoysk <dzikoysk@dzikoysk.net>
2023-07-04 23:22:11 +0200

    [events] Move events to `JavalinConfig` & convert `EventListener` to `EventConfig` (#1941)

---
+++ javalin/src/main/java/io/javalin/config/EventConfig.kt
@@ -0,0 +1,41 @@
+/*
+ * Javalin - https://javalin.io
+ * Copyright 2017 David Åse
+ * Licensed under Apache 2.0: https://github.com/tipsy/javalin/blob/master/LICENSE
+ */
+
+package io.javalin.config;
+
+import io.javalin.event.LifecycleEventListener
+import io.javalin.event.EventManager;
+import io.javalin.event.HandlerMetaInfo
+import io.javalin.event.JavalinLifecycleEvent;
+import io.javalin.event.JavalinLifecycleEvent.SERVER_STARTED
+import io.javalin.event.JavalinLifecycleEvent.SERVER_STARTING
+import io.javalin.event.JavalinLifecycleEvent.SERVER_START_FAILED
+import io.javalin.event.JavalinLifecycleEvent.SERVER_STOPPED
+import io.javalin.event.JavalinLifecycleEvent.SERVER_STOPPING
+import io.javalin.event.JavalinLifecycleEvent.SERVER_STOP_FAILED
+import io.javalin.event.WsHandlerMetaInfo
+import java.util.function.Consumer
+
+class EventConfig {
+
+    @JvmField val eventManager = EventManager()
+
+    fun serverStarting(lifecycleEventListener: LifecycleEventListener) = eventManager.addLifecycleEvent(SERVER_STARTING, lifecycleEventListener)
+    fun serverStarted(lifecycleEventListener: LifecycleEventListener) = eventManager.addLifecycleEvent(SERVER_STARTED, lifecycleEventListener)
+    fun serverStartFailed(lifecycleEventListener: LifecycleEventListener) = eventManager.addLifecycleEvent(SERVER_START_FAILED, lifecycleEventListener)
+    fun serverStopFailed(lifecycleEventListener: LifecycleEventListener) = eventManager.addLifecycleEvent(SERVER_STOP_FAILED, lifecycleEventListener)
+    fun serverStopping(lifecycleEventListener: LifecycleEventListener) = eventManager.addLifecycleEvent(SERVER_STOPPING, lifecycleEventListener)
+    fun serverStopped(lifecycleEventListener: LifecycleEventListener) = eventManager.addLifecycleEvent(SERVER_STOPPED, lifecycleEventListener)
+
+    fun handlerAdded(callback: Consumer<HandlerMetaInfo>) {
+        this.eventManager.handlerAddedHandlers.add(callback);
+    }
+
+    fun wsHandlerAdded(callback: Consumer<WsHandlerMetaInfo>) {
+        eventManager.wsHandlerAddedHandlers.add(callback);
+    }
+
+}
