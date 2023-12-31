948e67041dd339fc22df25c63f81036b1c402a28
Alexander <65727384+GoldenGamerLP@users.noreply.github.com>
2023-12-31 16:36:31 +0100

    Updated LoadableData to Vue3
    Updated LoadableData class, to work again with Vue2 and Vue3.
    You have to use Vue.reactive for the class to be reactive again.

--- javalin/src/main/java/io/javalin/vue/LoadableData.kt
+++ javalin/src/main/java/io/javalin/vue/LoadableData.kt
@@ -3,60 +3,91 @@ package io.javalin.vue
 const val loadableDataScript = """
 
 <script nonce="@internalAddNonce">
-    class LoadableData {
-        constructor(url, options = {cache: false, errorCallback: null}) {
-            this._url = url;
-            this._errorCallback = options.errorCallback ?? null;
-            this.data = null;
-            this.loading = true;
-            this.refreshing = false;
-            this.loaded = false;
-            this.loadError = null;
-            this._load(options.cache ?? false); // initial load
-            this.addRefreshListener(); // listen for global refresh events
-        }
-        _load(cache = false) {
+    class BackendData {
+    /**
+     * Initializes the data from the backend.
+     * @param url - The url to fetch the data from.
+     * @param options - Options for the data. Currently only supports cache and errorCallback.
+     */
+    constructor(url, options = { cache: false, errorCallback: null }) {
+        this._url = url;
+        this._errorCallback = options.errorCallback ?? null;
+        this.data = null;
+        this.loading = false;
+        this.refreshing = false;
+        this.loaded = false;
+        this.loadError = null;
+        this.addRefreshListener();
+    }
+
+    /**
+     * Initializes the data from the backend.
+     * @param cache - If true, the data will be cached in local storage.
+     * @returns {Promise<void>} - Resolves when the data is loaded.
+     */
+    async init(cache = false) {
+        this.loading = true;
+        try {
             let cacheKey = "LoadableData:" + this._url;
             if (cache) {
                 this.data = JSON.parse(localStorage.getItem(cacheKey)) || null;
                 this.loaded = this.data !== null;
                 this.loading = this.loaded === false;
             }
-            fetch(this._url).then(res => {
-                if (res.ok) return res.json();
-                throw { code: res.status, text: res.statusText };
-            }).then(data => {
-                this.data = data;
-                this.loaded = true;
-                if (cache) {
-                    localStorage.setItem(cacheKey, JSON.stringify(data));
-                }
-            }).catch(error => {
-                this.loadError = error;
-                if (this._errorCallback !== null) {
-                    this._errorCallback(error);
-                }
-            }).finally(() => {
-                this.loading = false;
-                this.refreshing = false;
-            });
-        }
-        refresh() {
-            this.refreshing = true;
-            this._load(false); // refresh doesn't clear data, so no need to cache
-        }
-        refreshAll() {
-            window.dispatchEvent(new CustomEvent("loadabledata-refresh", {detail: this._url}));
-        }
-        invalidateCache() {
-            localStorage.removeItem("LoadableData:" + this._url);
-        }
-        addRefreshListener() {
-            window.addEventListener("loadabledata-refresh", e => {
-                if (this._url === e.detail) {
-                    this.refresh();
-                }
-            }, false);
+            const response = await fetch(this._url);
+            if (!response.ok) {
+                throw { code: response.status, text: response.statusText };
+            }
+            const data = await response.json();
+            this.data = data;
+            this.loaded = true;
+            if (cache) {
+                localStorage.setItem(cacheKey, JSON.stringify(data));
+            }
+        } catch (error) {
+            this.loadError = error;
+            if (this._errorCallback !== null) {
+                this._errorCallback(error);
+            }
+        } finally {
+            this.loading = false;
+            this.refreshing = false;
         }
     }
+
+    /**
+     * Refreshes the data from the backend.
+     * If the data is cached, it will be used until the new data is loaded.
+     * @param {boolean} boolean - If true, all instances of this data will be refreshed.
+     */
+    refresh(boolean = false) {
+        this.refreshing = true;
+        this.init(false); // refresh doesn't clear data, so no need to cache
+        if(boolean) {
+            this.refreshAll();
+        }
+    }
+
+    /**
+     * Refreshes all instances of this data.
+     */
+    refreshAll() {
+        window.dispatchEvent(new CustomEvent("loadabledata-refresh", { detail: this._url }));
+    }
+
+    /**
+     * Invalidates the cache for this data.
+     */
+    invalidateCache() {
+        localStorage.removeItem("LoadableData:" + this._url);
+    }
+
+    addRefreshListener() {
+        window.addEventListener("loadabledata-refresh", (e) => {
+            if (this._url === e.detail) {
+                this.refresh(false);
+            }
+        }, false);
+    }
+}
 </script>"""
