2f840f5a6a095727ef59567fe9ce877c204cbb7c
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2024-02-02 16:55:21 +0100

    Add failing test

--- javalin/src/test/java/io/javalin/staticfiles/TestStaticFilesEdgeCases.kt
+++ javalin/src/test/java/io/javalin/staticfiles/TestStaticFilesEdgeCases.kt
@@ -17,15 +17,26 @@ import okhttp3.Protocol.H2_PRIOR_KNOWLEDGE
 import okhttp3.Request
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory
 import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory
-import org.eclipse.jetty.server.HttpConfiguration
+import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory
 import org.eclipse.jetty.server.HttpConnectionFactory
+import org.eclipse.jetty.server.SecureRequestCustomizer
 import org.eclipse.jetty.server.ServerConnector
+import org.eclipse.jetty.server.SslConnectionFactory
 import org.eclipse.jetty.util.ssl.SslContextFactory
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 import java.net.ServerSocket
+import java.net.http.HttpClient
+import java.security.KeyManagementException
+import java.security.NoSuchAlgorithmException
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSession
+import javax.net.ssl.X509TrustManager
 import kotlin.io.path.Path
 
 class TestStaticFilesEdgeCases {
@@ -69,7 +80,7 @@ class TestStaticFilesEdgeCases {
     }
 
     @Test
-    fun `static files works on http2 `() {
+    fun `static files works on http2 no ssl `() {
         // test http1 first:
         val http1App = Javalin.create { it.staticFiles.add("/public") }
         TestUtil.test(http1App) { _, http ->
@@ -79,8 +90,7 @@ class TestStaticFilesEdgeCases {
         val port = ServerSocket(0).use { it.localPort }
         val http2App = Javalin.create {
             it.staticFiles.add("/public")
-            it.jetty.addConnector { server, connector ->
-                val httpConfiguration = HttpConfiguration()
+            it.jetty.addConnector { server, httpConfiguration ->
                 val http11 = HttpConnectionFactory(httpConfiguration)
                 val http2 = HTTP2CServerConnectionFactory(httpConfiguration)
                 ServerConnector(server, http11, http2).apply {
@@ -94,6 +104,36 @@ class TestStaticFilesEdgeCases {
             val response = http2client.newCall(Request.Builder().url(path).build()).execute()
             assertThat(response.body?.string()).contains("CSS works")
             assertThat(response.protocol).isEqualTo(H2_PRIOR_KNOWLEDGE)
+        }
+    }
+
+    @Test
+    fun `static files works on http2 with ssl and ALPN`() {
+        val port = ServerSocket(0).use { it.localPort }
+        val http2App = Javalin.create {
+            it.staticFiles.add("/public")
+            it.jetty.addConnector { server, httpConfiguration ->
+                httpConfiguration.addCustomizer(SecureRequestCustomizer(false))
+                val http11 = HttpConnectionFactory(httpConfiguration)
+                val http2 = HTTP2ServerConnectionFactory(httpConfiguration)
+                val alpn = ALPNServerConnectionFactory()
+                alpn.setDefaultProtocol(http11.protocol)
+                val sslContextFactory = SslContextFactory.Server().apply {
+                    keyStorePath = this::class.java.getResource("/keystore.jks")!!.toExternalForm()
+                    keyStorePassword = "localhost"
+                }
+                val tlsHttp2 = SslConnectionFactory(sslContextFactory, alpn.protocol)
+                ServerConnector(server, tlsHttp2, alpn, http2, http11).apply { this.port = port }
+            }
+        }
+        TestUtil.test(http2App) { app, http ->
+            val client = untrustedClient()
+            val emptyPath = "https://127.0.0.1:$port/idontexist.css"
+            val emptyResponse = client.newCall(Request.Builder().url(emptyPath).build()).execute()
+            assertThat(emptyResponse.code).isEqualTo(404)
+            val path = "https://127.0.0.1:$port/styles.css"
+            val response = client.newCall(Request.Builder().url(path).build()).execute()
+            assertThat(response.body.toString()).contains("CSS works")
         }
     }
 
@@ -117,6 +157,32 @@ class TestStaticFilesEdgeCases {
             val response = Unirest.get("https://localhost:$port/styles.css").asString()
             assertThat(response.body).contains("CSS works")
         }.stop()
+    }
+
+    /** Needed to accept self-signed certificates in okhttp...
+     */
+     fun untrustedClient(): OkHttpClient {
+         val trustAllCerts = arrayOf<X509TrustManager>(object : X509TrustManager {
+             override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+             override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+             override fun getAcceptedIssuers(): Array<X509Certificate> {
+                 return arrayOf()
+             }
+         })
+        val sslContext: SSLContext = try {
+            SSLContext.getInstance("SSL")
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        }
+        try {
+            sslContext.init(null, trustAllCerts, SecureRandom())
+        } catch (e: KeyManagementException) {
+            throw RuntimeException(e)
+        }
+        val newBuilder = OkHttpClient.Builder()
+        newBuilder.sslSocketFactory(sslContext.socketFactory, trustAllCerts[0])
+        newBuilder.hostnameVerifier { _: String?, _: SSLSession? -> true }
+        return newBuilder.build()
     }
 
 }
