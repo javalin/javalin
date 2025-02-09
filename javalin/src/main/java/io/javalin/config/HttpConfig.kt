6cc5948b7b9476309cc8faa6c166412de243c78f
Playacem <playacem@gmail.com>
2025-02-09 11:33:49 +0100

    [http] Add config option for response buffer size with a new default
    The new default is the same what Jetty has by default.

--- javalin/src/main/java/io/javalin/config/HttpConfig.kt
+++ javalin/src/main/java/io/javalin/config/HttpConfig.kt
@@ -17,6 +17,7 @@ class HttpConfig(private val cfg: JavalinConfig) {
     @JvmField var prefer405over404 = false
     @JvmField var strictContentTypes = false
     @JvmField var maxRequestSize = 1_000_000L // increase this or use inputstream to handle large requests
+    @JvmField var responseBufferSize = 32_768
     @JvmField var defaultContentType = ContentType.PLAIN
     @JvmField var asyncTimeout = 0L
     //@formatter:on
